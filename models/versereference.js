bf740bdb9dc8ae83a41adbbe9a71a9b4eb4fbd52
Tobias Klein <contact@tklein.info>
2019-02-02 18:09:32 +0100

    Updated licensing information

--- models/versereference.js
+++ models/versereference.js
@@ -1,3 +1,21 @@
+/* This file is part of Ezra Project.
+
+   Copyright (C) 2019 Tobias Klein <contact@tklein.info>
+
+   Ezra Project is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Ezra Project is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Ezra Project. See the file COPYING.
+   If not, see <http://www.gnu.org/licenses/>. */
+
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var VerseReference = sequelize.define('VerseReference', {
