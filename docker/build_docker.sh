02b10839b3d2b83b427489c210315f66d4f87b32
Tobias Klein <contact@tklein.info>
2019-06-15 11:25:04 +0200

    Added call of scripts for CentOS and Fedora

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,3 +2,5 @@
 
 docker build --build-arg HOST_USER=${USER} -t ubuntu1804.ezra:1.0 -f Ubuntu1804_Dockerfile .
 docker build --build-arg HOST_USER=${USER} -t ubuntu1904.ezra:1.0 -f Ubuntu1904_Dockerfile .
+docker build --build-arg HOST_USER=${USER} -t centos.ezra:1.0 -f CentOS_Dockerfile .
+docker build --build-arg HOST_USER=${USER} -t fedora.ezra:1.0 -f Fedora_Dockerfile .
