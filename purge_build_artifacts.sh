9e121dcbf39e3305fe1eaeaa6da4d1c11865313b
Tobias Klein <contact@tklein.info>
2019-07-07 09:29:08 +0200

    Purge script: Do not show error messages if files not found

--- purge_build_artifacts.sh
+++ purge_build_artifacts.sh
@@ -3,7 +3,7 @@
 rm -rf node_modules/node-sword-interface/sword
 rm -rf node_modules/node-sword-interface/sword_build
 rm -rf node_modules/sqlite3/deps
-rm node_modules/sqlite3/build/Release/sqlite3.a
+rm -f node_modules/sqlite3/build/Release/sqlite3.a
 
 find node_modules -name obj -exec rm -rf {} >/dev/null 2>&1 \;
 find node_modules -name obj.target -exec rm -rf {} >/dev/null 2>&1 \;
