b36f1f6193105561de60f7228a00e645174a4eb1
Tobias Klein <contact@tklein.info>
2019-07-07 15:28:26 +0200

    Do not delete sqlite3 deps, this is needed for rebuilt

--- purge_build_artifacts.sh
+++ purge_build_artifacts.sh
@@ -2,7 +2,6 @@
 
 rm -rf node_modules/node-sword-interface/sword
 rm -rf node_modules/node-sword-interface/sword_build
-rm -rf node_modules/sqlite3/deps
 rm -f node_modules/sqlite3/build/Release/sqlite3.a
 
 find node_modules -name obj -exec rm -rf {} >/dev/null 2>&1 \;
