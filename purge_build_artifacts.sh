299e3f1a86a79a61c6b33c3f6ae7f1954c8e3880
Tobias Klein <contact@tklein.info>
2019-07-08 22:28:22 +0200

    Added clean exit for purge script

--- purge_build_artifacts.sh
+++ purge_build_artifacts.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-rm -rf node_modules/node-sword-interface/sword
-rm -rf node_modules/node-sword-interface/sword_build
-rm -f node_modules/sqlite3/build/Release/sqlite3.a
+rm -rf node_modules/node-sword-interface/sword >/dev/null 2>&1
+rm -rf node_modules/node-sword-interface/sword_build >/dev/null 2>&1
+rm -f node_modules/sqlite3/build/Release/sqlite3.a >/dev/null 2>&1
 
 find node_modules -name obj -exec rm -rf {} >/dev/null 2>&1 \;
 find node_modules -name obj.target -exec rm -rf {} >/dev/null 2>&1 \;
+
+exit 0
