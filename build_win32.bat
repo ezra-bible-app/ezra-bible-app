bfd838d052e34521b29bd76e5b76f20629597eba
Tobias Klein <contact@tklein.info>
2019-07-26 20:17:55 +0200

    build_win32.bat / Added command to copy the static Sword dlls to the release directory of node-sword-interface

--- build_win32.bat
+++ build_win32.bat
@@ -1,3 +1,4 @@
 call npm install electron@4.2.6 --arch=ia32
 call npm install --arch=ia32
 call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -w node-sword-interface -v 4.2.6
+call copy node_modules\node-sword-interface\build\sword-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
