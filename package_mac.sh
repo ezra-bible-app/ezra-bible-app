15bb609cfd978c4c719dc6e6af558e3c4c0c1fc4
Tobias Klein <contact@tklein.info>
2019-07-31 07:22:02 +0100

    Added entitlements options to mac packaging script

--- package_mac.sh
+++ package_mac.sh
@@ -4,7 +4,9 @@ echo "Enter your Apple ID"
 read APPLE_ID
 
 echo "Enter your Apple ID password:"
-read -s APPLE_ID_PW
+read APPLE_ID_PW
+
+echo ""
 
 rm -rf node_modules/node-sword-interface/sword node_modules/node-sword-interface/sword_build
 
@@ -13,5 +15,7 @@ node_modules/.bin/electron-packager . 'Ezra Project' --overwrite --platform=darw
 --app-bundle-id='net.ezra-project.electronapp' \
 --app-category-type='public.app-category.education' \
 --osx-sign.hardened-runtime \
+--osx-sign.entitlements='./entitlements.plist' \
+--osx-sign.entitlements-inherit='./entitlements.plist' \
 --osx-notarize.appleId="${APPLE_ID}" \
 --osx-notarize.appleIdPassword="${APPLE_ID_PW}"
