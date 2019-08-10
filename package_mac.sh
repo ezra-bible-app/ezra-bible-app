5b220e7fa7a45790e25c58327b14b76664eb41f4
Tobias Klein <contact@tklein.info>
2019-08-10 11:00:26 +0200

    Added debug option for electron-osx-sign

--- package_mac.sh
+++ package_mac.sh
@@ -10,6 +10,8 @@ echo ""
 
 rm -rf node_modules/node-sword-interface/sword node_modules/node-sword-interface/sword_build
 
+export DEBUG=electron-osx-sign*
+
 node_modules/.bin/electron-packager . 'Ezra Project' --overwrite --platform=darwin --arch=x64 --prune=true --out=release --electron-version=4.2.0 \
 --executable-name='ezra-project' \
 --app-bundle-id='net.ezra-project.electronapp' \
