c411401197a19c16d070b12f23cf90a595bf8174
Tobias Klein <contact@tklein.info>
2019-08-18 12:38:35 +0200

    Upgraded Electron from 4.2.6 to 4.2.9

--- build_win32.bat
+++ build_win32.bat
@@ -1,4 +1,4 @@
-call npm install electron@4.2.6 --arch=ia32
+call npm install electron@4.2.9 --arch=ia32
 call npm install --arch=ia32
-call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -w node-sword-interface -v 4.2.6
+call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -w node-sword-interface -v 4.2.9
 call copy node_modules\node-sword-interface\build\sword-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
--- package_mac.sh
+++ package_mac.sh
@@ -2,7 +2,7 @@
 
 rm -rf node_modules/node-sword-interface/sword node_modules/node-sword-interface/sword_build
 
-node_modules/.bin/electron-packager . 'Ezra Project' --overwrite --platform=darwin --arch=x64 --prune=true --out=release --electron-version=4.2.0 \
+node_modules/.bin/electron-packager . 'Ezra Project' --overwrite --platform=darwin --arch=x64 --prune=true --out=release --electron-version=4.2.9 \
 --executable-name='ezra-project' \
 --app-bundle-id='net.ezra-project.electronapp' \
 --app-category-type='public.app-category.education'
