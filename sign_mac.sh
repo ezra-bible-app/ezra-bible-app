e94bfb6ac7475334ea0fbb2fb9ca53a0c1d513f1
Tobias Klein <contact@tklein.info>
2019-08-23 15:22:25 +0200

    Added option to disable gatekeeper-assess option when signing binaries, because this didn't work on macOS Mojave

--- sign_mac.sh
+++ sign_mac.sh
@@ -5,5 +5,6 @@ export DEBUG=electron-osx-sign*
 node_modules/.bin/electron-osx-sign ./release/Ezra\ Project-darwin-x64/Ezra\ Project.app \
 --type='distribution' \
 --hardened-runtime \
+--no-gatekeeper-assess \
 --entitlements='./entitlements.plist' \
 --entitlements-inherit='./entitlements.plist'
