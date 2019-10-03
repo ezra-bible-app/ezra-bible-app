22892bb57f0236c82d7a0e219c0866862d440d6e
Tobias Klein <contact@tklein.info>
2019-10-03 19:01:18 +0200

    One more fix for cleanup_gyp_shebang.sh

--- cleanup_gyp_shebang.sh
+++ cleanup_gyp_shebang.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-find node_modules/node-gyp/gyp -name *.py -exec sed -i 's/\/usr\/bin\/env python/\/usr\/bin\/env python2/g' {} \;
-sed -i 's/\/usr\/bin\/env python/\/usr\/bin\/env python2/g' node_modules/node-gyp/gyp/samples/samples
+if [ -f /usr/bin/python2 ]
+then
+    find node_modules/node-gyp/gyp -name *.py -exec sed -i 's/\/usr\/bin\/env python$/\/usr\/bin\/env python2/g' {} \;
+    sed -i 's/\/usr\/bin\/python$/\/usr\/bin\/python2/g' node_modules/node-gyp/gyp/samples/samples
+fi
 
 exit 0
