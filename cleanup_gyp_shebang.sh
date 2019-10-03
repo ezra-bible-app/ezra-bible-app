c22c2d65892b1977b98d67e4669cc34c7cdbad8e
Tobias Klein <contact@tklein.info>
2019-10-03 18:47:07 +0200

    Added one more file to the cleanup_gyp_shebang.sh script

--- cleanup_gyp_shebang.sh
+++ cleanup_gyp_shebang.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 find node_modules/node-gyp/gyp -name *.py -exec sed -i 's/\/usr\/bin\/env python/\/usr\/bin\/env python2/g' {} \;
+sed -i 's/\/usr\/bin\/env python/\/usr\/bin\/env python2/g' node_modules/node-gyp/gyp/samples/samples
 
 exit 0
