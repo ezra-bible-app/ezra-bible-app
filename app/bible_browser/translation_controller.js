79a031226d7f384ab9e28c1ef6c3e431575feef0
Tobias Klein <contact@tklein.info>
2019-10-03 19:26:24 +0200

    Escape html for help-text-no-translations

--- app/bible_browser/translation_controller.js
+++ app/bible_browser/translation_controller.js
@@ -111,7 +111,7 @@ class TranslationController {
       bibleSelect.attr('disabled','disabled');
       $('.book-select-button').addClass('ui-state-disabled');
       var currentVerseList = bible_browser_controller.getCurrentVerseList(tabIndex);
-      currentVerseList.find('.help-text').text(i18n.t("help.help-text-no-translations"));
+      currentVerseList.find('.help-text').text(i18n.t("help.help-text-no-translations", { 'interpolation': {'escapeValue': false} }));
     } else {
       $('.bible-select').removeAttr('disabled');
       $('.book-select-button').removeClass('ui-state-disabled');
