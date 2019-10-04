5dbd2001f339b649b37bfcb3eae0e11d01e07e6f
Tobias Klein <contact@tklein.info>
2019-10-04 17:12:02 +0200

    Added migration code to eliminate duplicate VerseTags

--- migrations/20190908070503-addUniqueIndexToVerseTags.js
+++ migrations/20190908070503-addUniqueIndexToVerseTags.js
@@ -2,8 +2,34 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    // TODO: Remove duplicates
-    return queryInterface.addIndex('VerseTags', { fields: ['verseReferenceId', 'tagId'], unique: true });
+    var duplicatesQuery = "SELECT GROUP_CONCAT(id) AS duplicateIds, tagId, verseReferenceId, COUNT(*) AS tagCount " +
+                          "FROM VerseTags GROUP BY verseReferenceId, tagId ORDER BY tagCount DESC";
+    
+    return queryInterface.sequelize.query(duplicatesQuery).then(duplicates => {
+      var allQueries = [];
+
+      // This loop generates queries to delete duplicate VerseTags
+      for (var i = 0; i < duplicates[0].length; i++) {
+        var currentRecord = duplicates[0][i];
+        var tagCount = currentRecord.tagCount;
+        var duplicateIds = currentRecord.duplicateIds;
+
+        if (tagCount > 1) {
+          var ids = duplicateIds.split(',');
+          ids.shift();
+
+          var query = "DELETE FROM VerseTags WHERE id IN (" + ids.join(',') + ")";
+          var deleteAction = queryInterface.sequelize.query(query);
+          allQueries.push(deleteAction);
+        }
+      }
+
+      allQueries.push(queryInterface.addIndex('VerseTags', { fields: ['verseReferenceId', 'tagId'], unique: true }));
+
+      return Promise.all(
+        allQueries
+      );
+    });
   },
 
   down: (queryInterface, Sequelize) => {
