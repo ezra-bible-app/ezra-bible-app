43d9166aa5ad57acbededf11a70b0cef4c56fa55
Tobias Klein <contact@tklein.info>
2019-10-27 20:21:30 +0100

    Added a migration for re-importing all existing bible translations with Strong's (necessary based on latest node-sword-interface changes)

---
+++ migrations/20191027174828-migrateStrongsTranslations.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const NodeSwordInterface = require('node-sword-interface');
+    const nsi = new NodeSwordInterface();
+    const models = require('../models')(dbDir);
+
+    var localSwordModules = nsi.getAllLocalModules();
+    var dbModules = await models.BibleTranslation.findAndCountAll();
+
+    for (var dbTranslation of dbModules.rows) {
+      if (dbTranslation.hasStrongs) {
+        var swordModuleFound = false;
+
+        for (var i = 0; i < localSwordModules.length; i++) {
+          if (localSwordModules[i].name == dbTranslation.id) {
+            swordModuleFound = true;
+            break;
+          }
+        }
+
+        if (swordModuleFound) {
+          console.log("Re-synchronizing translation: " + dbTranslation.name);
+
+          await models.Verse.destroy({
+            where: {
+              bibleTranslationId: dbTranslation.id
+            }
+          });
+
+          await models.BibleTranslation.importSwordTranslation(dbTranslation.id, models);
+        }
+      }
+    }
+  },
+
+  down: (queryInterface, Sequelize) => {
+  }
+};
