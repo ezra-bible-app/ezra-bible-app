dd3bb1b389e2fc3a62f4a0cf0018e70a202c5d02
Tobias Klein <contact@tklein.info>
2019-11-01 19:32:09 +0100

    build.sh: Prune node modules after rebuild

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -2,7 +2,9 @@
 # This script triggers all the steps necessary for building and packaging Ezra Project on Linux.
 
 npm install
+npm run install-node-prune
 npm run rebuild
+npm run prune-nude-modules
 rm -rf /tmp/electron-packager &> /dev/null
 npm run purge-build-artifacts
 npm run cleanup-gyp-shebang
