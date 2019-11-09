0c4f913e6e62b4859e7d249f61ecc65446c36f1b
Tobias Klein <contact@tklein.info>
2019-11-09 18:23:49 +0100

    Add a script to update loc metrics

---
+++ LOC_METRICS.md
@@ -0,0 +1,20 @@
+# Ezra Project LOC Metrics
+
+Language|files|blank|comment|code
+:-------|-------:|-------:|-------:|-------:
+JavaScript|55|1917|1047|7260
+CSS|7|433|223|2087
+C++|8|389|219|1517
+Markdown|7|233|0|601
+JSON|10|0|0|568
+HTML|10|66|18|369
+C/C++ Header|7|130|91|336
+Bourne Shell|10|39|43|119
+Pug|1|8|3|67
+DOS Batch|2|0|0|5
+--------|--------|--------|--------|--------
+SUM:|117|3215|1644|12929
+
+The above metrics include both the frontend (mostly JavaScript) and the backend (mostly C++).
+
+Generated on Sat Nov  9 18:22:37 CET 2019 using [cloc](https://github.com/AlDanial/cloc).
---
+++ build_scripts/update_metrics.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo '# Ezra Project LOC Metrics' > LOC_METRICS.md
+cloc --md app \
+          build_scripts \
+          css \
+          html \
+          locales \
+          models \
+          migrations \
+          package_config \
+          seeders \
+          templates \
+          index.html \
+          main.js \
+          node_modules/node-sword-interface/src \
+          node_modules/node-sword-interface/*.md \
+          node_modules/node-sword-interface/*.sh \
+          node_modules/node-sword-interface/*.bat \
+          node_modules/node-sword-interface/*.js \
+          *.md | tail -n +4 >> LOC_METRICS.md
+
+echo "" >> LOC_METRICS.md
+echo "The above metrics include both the frontend (mostly JavaScript) and the backend (mostly C++)." >> LOC_METRICS.md
+
+DATE=`date`
+echo "" >> LOC_METRICS.md
+printf "Generated on ${DATE} using [cloc](https://github.com/AlDanial/cloc)" >> LOC_METRICS.md
