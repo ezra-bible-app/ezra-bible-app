f93306e426febb58e31da90b8cd63d15f067ee80
Tobias Klein <contact@tklein.info>
2019-11-30 15:53:37 +0100

    Use actual build script for build_ubuntu1804 action instead of symlink

--- .github/actions/build_ubuntu1804/build.sh
+++ .github/actions/build_ubuntu1804/build.sh
@@ -1,1 +1,11 @@
-../../../build_scripts/build.sh
+#!/bin/sh
+# This script triggers all the steps necessary for building and packaging Ezra Project on Linux.
+
+npm install
+npm run install-node-prune
+npm run rebuild
+npm run prune-node-modules
+rm -rf /tmp/electron-packager &> /dev/null
+npm run purge-build-artifacts
+npm run cleanup-gyp-shebang
+npm run package-linux
