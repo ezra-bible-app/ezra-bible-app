dc76ae60db14e487504e567decf66587fdda4ba2
Tobias Klein <contact@tklein.info>
2019-11-30 18:10:00 +0100

    Add GitHub build action for OpenSuse Leap and integrate into build workflow

---
+++ .github/actions/build_opensuseleap/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# This script triggers all the steps necessary for building and packaging Ezra Project on Linux.
+
+npm run build-unix
+npm run rpm_opensuse_leap
+cp release/packages/*.rpm $GITHUB_WORKSPACE/ezra-project_latest.rpm
