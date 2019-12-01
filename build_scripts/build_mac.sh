33bd4fc9541925391bc80ee5806bffaaba3671b8
Tobias Klein <contact@tklein.info>
2019-12-01 19:03:33 +0100

    Fix GitHub win32 build and add macOS build

---
+++ build_scripts/build_mac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script triggers all the steps necessary for building and packaging Ezra Project on macOS.
+
+npm install
+npm run compile-pug
+npm run install-node-prune
+npm run rebuild
+npm run prune-node-modules
+npm run purge-build-artifacts
+npm run package-mac
+npm run dmg-mac
