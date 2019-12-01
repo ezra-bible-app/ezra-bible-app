d17fcb035d860dce74f80039e811d4ff2500142f
Tobias Klein <contact@tklein.info>
2019-12-01 19:03:33 +0100

    Fix GitHub win32 build and add macOS build

--- .github/actions/build_centos8/build.sh
+++ .github/actions/build_centos8/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This script triggers all the steps necessary for building and packaging Ezra Project on Linux.
 
-npm run build-unix
+npm run build-linux
 npm run rpm_centos
 cp release/packages/*.rpm $GITHUB_WORKSPACE/ezra-project_latest.rpm
