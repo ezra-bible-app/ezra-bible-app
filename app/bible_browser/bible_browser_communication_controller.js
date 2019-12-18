76587c6e682db4ebb822eb49c62aa462183442a7
Tobias Klein <contact@tklein.info>
2019-12-18 08:55:49 +0100

    Refactoring: Move function BibleBrowserCommunicationController.request_book_text to TextLoader.requestBookText

--- app/bible_browser/bible_browser_communication_controller.js
+++ app/bible_browser/bible_browser_communication_controller.js
@@ -20,44 +20,6 @@ class BibleBrowserCommunicationController {
   constructor() {
   }
 
-  async request_book_text(tab_index,
-                          current_tab_id,
-                          book_short_title,
-                          render_function,
-                          start_verse_number=0,
-                          number_of_verses=0) {
-
-    var currentBibleTranslationId = bible_browser_controller.tab_controller.getTab(tab_index).getBibleTranslationId();
-
-    if (currentBibleTranslationId == null || 
-        currentBibleTranslationId == "") {
-
-      $('#verse-list-loading-indicator').hide();
-      return;
-    }
-
-    var bibleBook = await models.BibleBook.findOne({ where: { shortTitle: book_short_title }});
-    var verses = await bibleBook.getVerses(currentBibleTranslationId,
-                                           start_verse_number,
-                                           number_of_verses);
-
-    var verseTags = await bibleBook.getVerseTags(currentBibleTranslationId);
-    var groupedVerseTags = models.VerseTag.groupVerseTagsByVerse(verseTags);
-
-    var verses_as_html = verseListTemplate({
-      verseListId: current_tab_id,
-      renderVerseMetaInfo: true,
-      renderBibleBookHeaders: false,
-      bibleBooks: [bibleBook],
-      verses: verses,
-      verseTags: groupedVerseTags,
-      reference_separator: reference_separator,
-      tagHint: i18n.t("bible-browser.tag-hint")
-    });
-
-    render_function(verses_as_html);
-  }
-
   async request_verses_for_search_results(tab_index,
                                           current_tab_id,
                                           search_results,
