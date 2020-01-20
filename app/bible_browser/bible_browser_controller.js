f853be4667c825386002caa30a6d4b8eadaa1d6b
Tobias Klein <contact@tklein.info>
2020-01-20 07:31:03 +0100

    BibleBrowserController: Remove time measurements

--- app/bible_browser/bible_browser_controller.js
+++ app/bible_browser/bible_browser_controller.js
@@ -134,7 +134,6 @@ class BibleBrowserController {
   }
 
   async onTabSelected(event = undefined, ui = { 'index' : 0}) {
-    console.time('onTabSelected');
     // The ui.index may be higher as the actual available index. This happens after a tab was removed.
     if (ui.index > (this.tab_controller.getTabCount() - 1)) {
       // In this case we simply adjust the index to the last available index.
@@ -190,11 +189,9 @@ class BibleBrowserController {
     this.verse_context_loader.hide_verse_expand_box();
 
     configure_button_styles('.verse-list-menu');
-    console.timeEnd('onTabSelected');
   }
 
   onTabAdded(tabIndex=0) {
-    console.time('onTabAdded');
     this.hideAllMenus();
     // Refresh the view based on the options selected
     this.optionsMenu.refreshViewBasedOnOptions(tabIndex);
@@ -213,7 +210,6 @@ class BibleBrowserController {
 
     this.optionsMenu.initCurrentOptionsMenu(tabIndex);
     this.book_selection_menu.clearSelectedBookInMenu();
-    console.timeEnd('onTabAdded');
   }
 
   onBibleTranslationChanged() {
