108540a344527ec35c200f31a1a793894d9be231
Tobias Klein <contact@tklein.info>
2020-01-26 19:45:03 +0100

    Change name of license file in file headers (LICENSE instead of COPYING)

--- app/helpers/language_mapper.js
+++ app/helpers/language_mapper.js
@@ -13,7 +13,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with Ezra Project. See the file COPYING.
+   along with Ezra Project. See the file LICENSE.
    If not, see <http://www.gnu.org/licenses/>. */
 
 const langs = require('iso-639-3');
