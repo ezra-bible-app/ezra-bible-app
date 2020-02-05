5c32ea1cfe6794335e49db4f2a2591172f82c76b
Tobias Klein <contact@tklein.info>
2020-02-05 08:32:50 +0100

    Upgrade Electron from 7.1.7 to 8.0.0

--- build_scripts/build_win32.bat
+++ build_scripts/build_win32.bat
@@ -1,6 +1,6 @@
-call npm install electron@7.1.7 --arch=ia32
+call npm install electron@8.0.0 --arch=ia32
 call npm install --arch=ia32
-call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -w node-sword-interface -v 7.1.7
+call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -w node-sword-interface -v 8.0.0
 call copy node_modules\node-sword-interface\build\sword-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
 call npm run package-win
 call npm run installer-win
