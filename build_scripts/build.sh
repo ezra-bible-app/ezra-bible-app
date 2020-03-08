265ec6ffd16ccfe52a5aa898af68c70865fc9656
Tobias Klein <contact@tklein.info>
2020-03-08 08:43:24 +0100

    Only run cleanup-gyp-shebang on rpm-based systems

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -8,5 +8,13 @@ npm run rebuild
 npm run prune-node-modules
 rm -rf /tmp/electron-packager &> /dev/null
 npm run purge-build-artifacts
-npm run cleanup-gyp-shebang
+
+if [ -n "$(rpm -qa)" ] 2>/dev/null; then
+  # The rpm package database has some content, we assume we're on an rpm-based system.
+  # Here we need to clean up the gyp shebangs, because otherwise we will get these error messages during packaging:
+  # *** ERROR: ambiguous python shebang in /usr/lib/ezra-project/resources/app/node_modules/node-gyp/gyp/samples/samples: #!/usr/bin/python.
+  # Change it to python3 (or python2) explicitly.
+  npm run cleanup-gyp-shebang
+fi
+
 npm run package-linux
