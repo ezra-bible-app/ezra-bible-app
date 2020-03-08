4b949809c9e6ea4722b901690e57dc643b61dbea
Tobias Klein <contact@tklein.info>
2020-03-08 09:00:44 +0100

    Check if rpm is available, before triggering the rpm database

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -9,12 +9,14 @@ npm run prune-node-modules
 rm -rf /tmp/electron-packager &> /dev/null
 npm run purge-build-artifacts
 
-if [ -n "$(rpm -qa)" ] 2>/dev/null; then
-  # The rpm package database has some content, we assume we're on an rpm-based system.
-  # Here we need to clean up the gyp shebangs, because otherwise we will get these error messages during packaging:
-  # *** ERROR: ambiguous python shebang in /usr/lib/ezra-project/resources/app/node_modules/node-gyp/gyp/samples/samples: #!/usr/bin/python.
-  # Change it to python3 (or python2) explicitly.
-  npm run cleanup-gyp-shebang
+if which rpm >/dev/null; then
+    if [ -n "$(rpm -qa)" ] 2>/dev/null; then
+        # The rpm package database has some content, we assume we're on an rpm-based system.
+        # Here we need to clean up the gyp shebangs, because otherwise we will get these error messages during packaging:
+        # *** ERROR: ambiguous python shebang in /usr/lib/ezra-project/resources/app/node_modules/node-gyp/gyp/samples/samples: #!/usr/bin/python.
+        # Change it to python3 (or python2) explicitly.
+        npm run cleanup-gyp-shebang
+    fi
 fi
 
 npm run package-linux
