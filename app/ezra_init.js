414a4e46055a379346ce0137ca8dbfa9b8cc5a7c
Tobias Klein <contact@tklein.info>
2020-04-02 08:47:58 +0200

    Simplify function create_xml_doc

--- app/ezra_init.js
+++ app/ezra_init.js
@@ -75,16 +75,7 @@ function sleep(time) {
 
 $.create_xml_doc = function(string)
 {
-  var doc;
-
-  if ($.browser.msie) {
-    doc = new ActiveXObject('Microsoft.XMLDOM');
-    doc.async = 'false'
-    doc.loadXML(string);
-  } else {
-    doc = (new DOMParser()).parseFromString(string, 'text/xml');
-  }
-
+  var doc = (new DOMParser()).parseFromString(string, 'text/xml');
   return doc;
 }
 
