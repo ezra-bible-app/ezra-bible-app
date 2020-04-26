99b74a1e8a364b03006976f18cc4d8a6be7a82dd
Tobias Klein <contact@tklein.info>
2020-04-26 16:50:58 +0200

    Tag lists: Properly render lastUsed data for tags

--- models/tag.js
+++ models/tag.js
@@ -35,7 +35,7 @@ module.exports = (sequelize, DataTypes) => {
                  " SUM(CASE WHEN vt.tagId IS NULL THEN 0 ELSE 1 END) AS globalAssignmentCount," +
                  " SUM(CASE WHEN vr.bibleBookId=" + bibleBookId + " THEN 1 ELSE 0 END) AS bookAssignmentCount";
 
-    if (!onlyStats) query += ", strftime('%s', MAX(vt.updatedAt)) AS lastUsed";
+    query += ", strftime('%s', MAX(vt.updatedAt)) AS lastUsed";
 
     query += " FROM Tags t" +
              " LEFT JOIN VerseTags vt ON vt.tagId = t.id" +
