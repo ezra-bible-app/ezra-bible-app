84133ec062e709a23b616588e03abe46fc9cb8ac
Tobias Klein <contact@tklein.info>
2020-06-07 09:48:59 +0200

    build_win32.bat: Upload binaries to Sentry

--- build_scripts/build_win32.bat
+++ build_scripts/build_win32.bat
@@ -6,3 +6,10 @@ call npm run install-node-prune-win
 call npm run prune-node-modules
 call npm run package-win
 call npm run installer-win
+
+if "%GITHUB_EVENT_NAME%"=="push" (
+  node_modules\.bin\sentry-cli --auth-token %SENTRY_TOKEN% upload-dif -o tobias-klein -p ezra-project node_modules\node-sword-interface\build\Release\node_sword_interface.node
+  node_modules\.bin\sentry-cli --auth-token %SENTRY_TOKEN% upload-dif -o tobias-klein -p ezra-project node_modules\node-sword-interface\build\Release\node_sword_interface.pdb
+  node_modules\.bin\sentry-cli --auth-token %SENTRY_TOKEN% upload-dif -o tobias-klein -p ezra-project node_modules\node-sword-interface\build\sword-build-win32\lib\sword.lib
+  node_modules\.bin\sentry-cli --auth-token %SENTRY_TOKEN% upload-dif -o tobias-klein -p ezra-project node_modules\node-sword-interface\build\sword-build-win32\lib\sword.dll
+)
