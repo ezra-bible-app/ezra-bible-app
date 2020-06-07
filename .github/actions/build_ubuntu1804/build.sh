15c92282abcb2e546ac3b5617589446fa809d111
Tobias Klein <contact@tklein.info>
2020-06-07 08:46:59 +0200

    Delete build.sh files from GitHub actions directory, they are part of a separate repo ezra-project-docker since a while!

--- .github/actions/build_ubuntu1804/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This script triggers all the steps necessary for building and packaging Ezra Project on Linux.
-
-npm run build-linux
-npm run deb_1804
-cp release/packages/*.deb $GITHUB_WORKSPACE/ezra-project_latest.deb
--- .github/actions/build_ubuntu2004/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This script triggers all the steps necessary for building and packaging Ezra Project on Linux.
-
-npm run build-linux
-npm run deb_2004
-cp release/packages/*.deb $GITHUB_WORKSPACE/ezra-project_latest.deb
