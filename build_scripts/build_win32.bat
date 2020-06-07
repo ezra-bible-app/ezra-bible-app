747f12850533e1dd404643952e691878fb5e64bc
Tobias Klein <contact@tklein.info>
2020-06-07 10:15:11 +0200

    build_win32.bat: Upload node-sword-interface binaries to Sentry in case of release event

--- build_scripts/build_win32.bat
+++ build_scripts/build_win32.bat
@@ -7,9 +7,7 @@ call npm run prune-node-modules
 call npm run package-win
 call npm run installer-win
 
-if "%GITHUB_EVENT_NAME%"=="push" (
+if "%GITHUB_EVENT_NAME%"=="release" (
   node_modules\.bin\sentry-cli --auth-token %SENTRY_TOKEN% upload-dif -o tobias-klein -p ezra-project node_modules\node-sword-interface\build\Release\node_sword_interface.node
   node_modules\.bin\sentry-cli --auth-token %SENTRY_TOKEN% upload-dif -o tobias-klein -p ezra-project node_modules\node-sword-interface\build\Release\node_sword_interface.pdb
-  node_modules\.bin\sentry-cli --auth-token %SENTRY_TOKEN% upload-dif -o tobias-klein -p ezra-project node_modules\node-sword-interface\build\sword-build-win32\lib\sword.lib
-  node_modules\.bin\sentry-cli --auth-token %SENTRY_TOKEN% upload-dif -o tobias-klein -p ezra-project node_modules\node-sword-interface\build\sword-build-win32\lib\sword.dll
 )
