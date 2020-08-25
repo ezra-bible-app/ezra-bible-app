114f42dab38f4cfc108f6840382e3c8b278c518e
Tobias Klein <contact@tklein.info>
2020-08-25 08:46:13 +0200

    Do not check for installation of Python 2 when cleaning up gyp shebang

--- build_scripts/cleanup_gyp_shebang.sh
+++ build_scripts/cleanup_gyp_shebang.sh
@@ -3,10 +3,7 @@
 # The package node-gyp (Node.js native addon build tool) still depends on Python 2. However, this causes problems with rpm-build.
 # The following snippet basically changes all plain Python shebangs of the node-gyp Python scripts to a specific Python 2 shebang
 
-if [ -f /usr/bin/python2 ]
-then
-    find node_modules/node-gyp/gyp -name *.py -exec sed -i 's/\/usr\/bin\/env python$/\/usr\/bin\/env python2/g' {} \;
-    sed -i 's/\/usr\/bin\/python$/\/usr\/bin\/python2/g' node_modules/node-gyp/gyp/samples/samples
-fi
+find node_modules/node-gyp/gyp -name *.py -exec sed -i 's/\/usr\/bin\/env python$/\/usr\/bin\/env python2/g' {} \;
+sed -i 's/\/usr\/bin\/python$/\/usr\/bin\/python2/g' node_modules/node-gyp/gyp/samples/samples
 
 exit 0
