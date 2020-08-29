2740e6b2d285f98ebfd8622444691633c741de6a
Tobias Klein <contact@tklein.info>
2020-08-29 10:29:52 +0200

    build_win32.bat / Rebuild sqlite3 and node-sword-interface separately

--- build_scripts/build_win32.bat
+++ build_scripts/build_win32.bat
@@ -1,6 +1,9 @@
 call npm install electron@9.1.0 --arch=ia32
 call npm install --arch=ia32
-call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -w node-sword-interface -v 9.1.0
+echo "Rebuilding sqlite3"
+call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -o sqlite3 -v 9.1.0
+echo "Rebuilding node-sword-interface"
+call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -o node-sword-interface -v 9.1.0
 call copy node_modules\node-sword-interface\build\sword-build-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
 call npm run install-node-prune-win
 call npm run prune-node-modules
