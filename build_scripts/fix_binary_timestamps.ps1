51de5d216b6129c01d0aeb2f97b1926e744faf6d
Tobias Klein <contact@tklein.info>
2020-09-27 15:54:38 +0200

    fix_binary_timestamps: Fix code formatting

--- build_scripts/fix_binary_timestamps.ps1
+++ build_scripts/fix_binary_timestamps.ps1
@@ -1,1 +1,7 @@
-gci -path "release\\" -rec -file *.* | Where-Object {$_.LastWriteTime -lt (Get-Date).AddYears(-20)} | %  { try { $_.LastWriteTime = '01/01/2020 00:00:00' } catch {} }
+gci -path "release\\" -rec -file *.* |
+    Where-Object {$_.LastWriteTime -lt (Get-Date).AddYears(-20)} |
+        %  {
+            try { 
+                $_.LastWriteTime = '01/01/2020 00:00:00'
+            } catch {}
+        }
