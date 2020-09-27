f491566b81a6a5375216012348172724042c45c8
Tobias Klein <contact@tklein.info>
2020-09-27 17:04:42 +0200

    Add explanation to script fix_binary_timestamps.ps1

--- build_scripts/fix_binary_timestamps.ps1
+++ build_scripts/fix_binary_timestamps.ps1
@@ -1,6 +1,14 @@
-gci -path "release\\" -rec -file *.* |
+<#
+This PowerShell script is used as a work-around to address a bug in NuGet that consequently
+causes a packing problem with electron-installer-windows.
+
+see https://github.com/electron-userland/electron-installer-windows/issues/327
+see https://github.com/NuGet/Home/issues/7001
+#>
+
+Get-ChildItem -path "release\\" -rec -file *.* |
     Where-Object {$_.LastWriteTime -lt (Get-Date).AddYears(-20)} |
-        %  {
+        ForEach-Object  {
             try { 
                 $_.LastWriteTime = '01/01/2020 00:00:00'
             } catch {}
