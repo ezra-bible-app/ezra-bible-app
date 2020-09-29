cda41c9bb51c0ba255b0b2f42da29b3b5ab8ec26
Tobias Klein <contact@tklein.info>
2020-09-29 19:39:47 +0200

    Use sqlite 4.2.0 on Windows

--- build_scripts/build_win32.bat
+++ build_scripts/build_win32.bat
@@ -1,6 +1,6 @@
 REM On Windows we use Electron 8, because the latest sqlite3 (needed for Electron 9) does not run successfully yet.
 call npm install --arch=ia32
-REM call npm install electron@9.3.1 sqlite3@4.2.0 --arch=ia32
+call npm install sqlite3@4.2.0 --arch=ia32
 call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -w node-sword-interface -v 9.3.1
 call copy node_modules\node-sword-interface\build\sword-build-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
 call npm run install-node-prune-win
