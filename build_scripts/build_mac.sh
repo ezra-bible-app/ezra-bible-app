9cf27e16ebcd89a61f31a212d5aacc970193ecef
Tobias Klein <contact@tklein.info>
2020-10-11 10:02:21 +0200

    Add automatic sentry symbol upload for macos build

--- build_scripts/build_mac.sh
+++ build_scripts/build_mac.sh
@@ -8,3 +8,8 @@ npm run rebuild
 npm run prune-node-modules
 npm run purge-build-artifacts
 npm run package-mac
+
+if test "$GITHUB_EVENT_NAME" = "release"
+then
+  node_modules/.bin/sentry-cli --auth-token $SENTRY_TOKEN upload-dif -o tobias-klein -p ezra-project node_modules/node-sword-interface/build/Release/node_sword_interface.node
+fi
