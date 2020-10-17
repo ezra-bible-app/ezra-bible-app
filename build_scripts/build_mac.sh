d5d3193d4797e42c1b690b5eba8db52561aaac65
Tobias Klein <contact@tklein.info>
2020-10-17 20:37:38 +0200

    Add option MACOSX_DEPLOYMENT_TARGET to build script for mac OS

--- build_scripts/build_mac.sh
+++ build_scripts/build_mac.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # This script triggers all the steps necessary for building and packaging Ezra Project on macOS.
 
+# Set minimum macOS version to 10.10 (Yosemite)
+export MACOSX_DEPLOYMENT_TARGET=10.10
+
 npm install
 npm run compile-pug
 npm run install-node-prune
