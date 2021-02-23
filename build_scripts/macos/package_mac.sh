9c1cccbaaf5234c8fa14f28d7bd79ea6f9978482
Tobias Klein <contact@tklein.info>
2021-02-23 21:43:16 +0100

    Make some adjustments for macOS build

--- build_scripts/macos/package_mac.sh
+++ build_scripts/macos/package_mac.sh
@@ -2,7 +2,7 @@
 
 rm -rf node_modules/node-sword-interface/sword node_modules/node-sword-interface/sword_build
 
-node_modules/.bin/electron-packager . 'Ezra Bible App' --overwrite --platform=darwin --arch=x64 --prune=true --out=release --electron-version=9.2.1 \
+node_modules/.bin/electron-packager . ezra-bible-app --overwrite --platform=darwin --arch=x64 --prune=true --out=release --electron-version=9.2.1 \
 --executable-name='ezra-bible-app' \
 --app-bundle-id='net.ezrabibleapp.electron' \
 --app-category-type='public.app-category.education' \
