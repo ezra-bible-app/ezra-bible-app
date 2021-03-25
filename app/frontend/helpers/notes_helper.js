d59cc362f8174cbda531538af221a326031103f6
zhuiks <zhuke@ukr.net>
2021-03-25 15:01:48 -0500

    fix note icon title

--- app/frontend/helpers/notes_helper.js
+++ app/frontend/helpers/notes_helper.js
@@ -1,12 +1,13 @@
 
-const marked = require("marked");
+const removeMd = require('remove-markdown');
 
-const MAX_TOOLTIP_LENGTH = 256;
+const MAX_TOOLTIP_LENGTH = 100;
 
 
 function getTooltipText (noteText) {
   if(noteText && noteText.trim() !== "") {
-    return marked.parseInline(noteText.length > MAX_TOOLTIP_LENGTH ? noteText.slice(0, MAX_TOOLTIP_LENGTH) + "..." : noteText);
+    const deMarked = removeMd(noteText).replace(/\s*\n\s*/mg, ' - ');
+    return deMarked.length > MAX_TOOLTIP_LENGTH ? deMarked.slice(0, MAX_TOOLTIP_LENGTH) + "..." : deMarked;
   } else {
     return i18n.t('bible-browser.new-note-hint');
   }
