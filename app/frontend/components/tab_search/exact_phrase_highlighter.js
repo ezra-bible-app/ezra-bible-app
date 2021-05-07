98ed2553b6649bbd4febdab5862b07ab3d2f1158
zhuiks <zhuke@ukr.net>
2021-05-07 15:58:08 -0500

    refactor remove global escapeRegExp

--- app/frontend/components/tab_search/exact_phrase_highlighter.js
+++ app/frontend/components/tab_search/exact_phrase_highlighter.js
@@ -16,6 +16,8 @@
    along with Ezra Bible App. See the file LICENSE.
    If not, see <http://www.gnu.org/licenses/>. */
 
+const { escapeRegExp } = require('../../helpers/ezra_helper');
+
 class ExactPhraseHighlighter {
   constructor(highlightFunction) {
     this.highlightFunction = highlightFunction;
