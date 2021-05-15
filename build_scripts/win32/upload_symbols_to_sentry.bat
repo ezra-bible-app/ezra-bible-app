528160f91376e49302f2b2dfdcb83ac878e3a670
Tobias Klein <contact@tklein.info>
2021-05-15 12:30:22 +0200

    fix sentry upload

--- build_scripts/win32/upload_symbols_to_sentry.bat
+++ build_scripts/win32/upload_symbols_to_sentry.bat
@@ -1,4 +1,5 @@
-call node_modules\.bin\sentry-cli upload-dif -o tobias-klein -p ezra-bible-app node_modules\node-sword-interface\build\Release\node_sword_interface.node --auth-token %SENTRY_TOKEN%
-call node_modules\.bin\sentry-cli upload-dif -o tobias-klein -p ezra-bible-app node_modules\node-sword-interface\build\Release\sword.dll --auth-token %SENTRY_TOKEN%
-call node_modules\.bin\sentry-cli upload-dif -o tobias-klein -p ezra-bible-app node_modules\node-sword-interface\build\sword-build-win32\lib\node_sword_interface.pdb --auth-token %SENTRY_TOKEN%
-call node_modules\.bin\sentry-cli upload-dif -o tobias-klein -p ezra-bible-app node_modules\node-sword-interface\build\sword-build-win32\lib\sword.pdb --auth-token %SENTRY_TOKEN%
+set SENTRY_AUTH_TOKEN=%SENTRY_TOKEN%
+call node_modules\.bin\sentry-cli upload-dif -o tobias-klein -p ezra-bible-app node_modules\node-sword-interface\build\Release\node_sword_interface.node
+call node_modules\.bin\sentry-cli upload-dif -o tobias-klein -p ezra-bible-app node_modules\node-sword-interface\build\Release\sword.dll
+call node_modules\.bin\sentry-cli upload-dif -o tobias-klein -p ezra-bible-app node_modules\node-sword-interface\build\sword-build-win32\lib\node_sword_interface.pdb
+call node_modules\.bin\sentry-cli upload-dif -o tobias-klein -p ezra-bible-app node_modules\node-sword-interface\build\sword-build-win32\lib\sword.pdb
