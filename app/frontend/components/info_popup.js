86845d11aa5190f0a2e34e12d2bd99af745f5417
Augustin Colesnic <augustin@colesnic.net>
2021-05-27 21:02:47 +0300

    (chore) remove extra title and amend styling of table

--- app/frontend/components/info_popup.js
+++ app/frontend/components/info_popup.js
@@ -120,28 +120,25 @@ class InfoPopup {
 
       <div id='app-info-tabs-4' class='info-tabs scrollable'>
        <h2>${i18n.t("shortcuts.description")}</h2>
-        <table role="table">
+        <table role="table" id="info-table-shortcuts">
           <thead>
           <tr>
-            <th>Summary</th>
-            <th>Shortcut</th>
+            <th>${i18n.t("shortcuts.summary.column-title")}</th>
+            <th>${i18n.t("shortcuts.shortcut.column-title")}</th>
           </tr>
           </thead>
            
-          <tr><td>${i18n.t("shortcuts.summary.increase-current-font-size")}</td><td>${i18n.t("shortcuts.shortcut.increase-current-font-size")}</td></tr>
-          <tr><td>${i18n.t("shortcuts.summary.decrease-current-font-size")}</td><td>${i18n.t("shortcuts.shortcut.decrease-current-font-size")}</td></tr>
-          <tr><td>${i18n.t("shortcuts.summary.reset-font-size-to-default")}</td><td>${i18n.t("shortcuts.shortcut.reset-font-size-to-default")}</td></tr>
-   
-          <tr><td style='width: 15em;'>${i18n.t("shortcuts.application-version")}:</td><td>${version}</td></tr>
-          <tr><td>${i18n.t("shortcuts.summary.column-title")}</td><td>${i18n.t("shortcuts.shortcut.column-title")}</td></tr>
-          <tr><td>${i18n.t("shortcuts.summary.in-tab-search")}</td><td>${i18n.t("shortcuts.shortcut.in-tab-search")}</td></tr>
-          <tr><td>${i18n.t("shortcuts.summary.hide-in-tab-search-form")}</td><td>${i18n.t("shortcuts.shortcut.hide-in-tab-search-form")}</td></tr>
-          <tr><td>${i18n.t("shortcuts.summary.jump-to-next-search-occurance")}</td><td>${i18n.t("shortcuts.shortcut.jump-to-next-search-occurance")}</td></tr>
-          <tr><td>${i18n.t("shortcuts.summary.jump-to-previous-occurance")}</td><td>${i18n.t("shortcuts.shortcut.jump-to-previous-occurance")}</td></tr>
-          <tr><td>${i18n.t("shortcuts.summary.copy-selected-verses-to-clipboard")}</td><td>${i18n.t("shortcuts.shortcut.copy-selected-verses-to-clipboard")}</td></tr>
-          <tr><td>${i18n.t("shortcuts.summary.toggle-fullscreen-only-Win-Linux")}</td><td>${i18n.t("shortcuts.shortcut.toggle-fullscreen-only-Win-Linux")}</td></tr>
-          <tr><td>${i18n.t("shortcuts.summary.enable-dynamic-Strong-s-display")}</td><td>${i18n.t("shortcuts.shortcut.enable-dynamic-Strong-s-display")}</td></tr>
-          <tr><td>${i18n.t("shortcuts.summary.open-new-tab")}</td><td>${i18n.t("shortcuts.shortcut.open-new-tab")}</td></tr>
+          <tr><td>${i18n.t("shortcuts.summary.increase-current-font-size")}</td><td><code>${i18n.t("shortcuts.shortcut.increase-current-font-size")}</code></td></tr>
+          <tr><td>${i18n.t("shortcuts.summary.decrease-current-font-size")}</td><td><code>${i18n.t("shortcuts.shortcut.decrease-current-font-size")}</code></td></tr>
+          <tr><td>${i18n.t("shortcuts.summary.reset-font-size-to-default")}</td><td><code>${i18n.t("shortcuts.shortcut.reset-font-size-to-default")}</code></td></tr>
+          <tr><td>${i18n.t("shortcuts.summary.in-tab-search")}</td><td><code>${i18n.t("shortcuts.shortcut.in-tab-search")}</code></td></tr>
+          <tr><td>${i18n.t("shortcuts.summary.hide-in-tab-search-form")}</td><td><code>${i18n.t("shortcuts.shortcut.hide-in-tab-search-form")}</code></td></tr>
+          <tr><td>${i18n.t("shortcuts.summary.jump-to-next-search-occurance")}</td><td><code>${i18n.t("shortcuts.shortcut.jump-to-next-search-occurance")}</code></td></tr>
+          <tr><td>${i18n.t("shortcuts.summary.jump-to-previous-occurance")}</td><td><code>${i18n.t("shortcuts.shortcut.jump-to-previous-occurance")}</code></td></tr>
+          <tr><td>${i18n.t("shortcuts.summary.copy-selected-verses-to-clipboard")}</td><td><code>${i18n.t("shortcuts.shortcut.copy-selected-verses-to-clipboard")}</code></td></tr>
+          <tr><td>${i18n.t("shortcuts.summary.toggle-fullscreen-only-Win-Linux")}</td><td><code>${i18n.t("shortcuts.shortcut.toggle-fullscreen-only-Win-Linux")}</code></td></tr>
+          <tr><td>${i18n.t("shortcuts.summary.enable-dynamic-Strong-s-display")}</td><td><code>${i18n.t("shortcuts.shortcut.enable-dynamic-Strong-s-display")}</code></td></tr>
+          <tr><td>${i18n.t("shortcuts.summary.open-new-tab")}</td><td><code>${i18n.t("shortcuts.shortcut.open-new-tab")}</code></td></tr>
          
         </table>
       </div>
