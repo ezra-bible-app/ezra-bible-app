a5a1cbee364de2b9044cd259fa3f662f8c7c3095
zhuiks <zhuke@ukr.net>
2021-06-08 11:43:42 -0500

    add JSDoc annotation

--- app/frontend/helpers/i18n_helper.js
+++ app/frontend/helpers/i18n_helper.js
@@ -16,6 +16,14 @@
    along with Ezra Bible App. See the file LICENSE.
    If not, see <http://www.gnu.org/licenses/>. */
 
+
+/**
+ * This module contains utility functions to get various locale specific data (internationalization functions)
+ * @module i18NHelper
+ * @category Utility
+ */
+
+
 const i18nController = require('../controllers/i18n_controller.js');
 const languageMapper = require('../../lib/language_mapper.js');
 
@@ -68,7 +76,14 @@ module.exports.getLocalizedDate = function(timestamp) {
   return new Date(Date.parse(timestamp)).toLocaleDateString(locale);
 }
 
-module.exports.getLanguageName = function(code, includeNativeName = false, currentLocale = null) {
+/**
+ * Function to get localized language name. Uses module:languageMapper.getLanguageName under the hood
+ * @param {string} code 2-letter ISO 639-1 or 3-letter ISO 639-2/T  or 3-letter language code
+ * @param {boolean} [includeNativeName=false] either to add an extra native name in parenthesis 
+ * @param {string} [currentLocale=null] language code to return language name in (language name localization). If not provided uses current app locale
+ * @returns {string} localized language name if available. Otherwise language name in default locale (English) or initial language code
+ */
+module.exports.getLanguageName = function(code, includeNativeName=false, currentLocale=null) {
   currentLocale = currentLocale || i18nController.getLocale();
   var localeName = languageMapper.getLanguageName(code, currentLocale);
 
@@ -76,7 +91,7 @@ module.exports.getLanguageName = function(code, includeNativeName = false, curre
     return localeName + (includeNativeName && code !== currentLocale ? ` (${languageMapper.getLanguageName(code, code)})` : '');
   }
 
-  localeName = languageMapper.getLanguageName(code); //get locale name without localization
+  localeName = languageMapper.getLanguageName(code); // get locale name without localization
 
   return localeName || code;
 }
--- app/lib/language_mapper.js
+++ app/lib/language_mapper.js
@@ -17,9 +17,21 @@
    If not, see <http://www.gnu.org/licenses/>. */
 
 
-const hasIntlDisplayNames = Intl && typeof Intl === "object" && typeof Intl.DisplayNames === "function";
+/**
+ * This module contains utility functions to get language name and details by ISO language code or vise versa
+ * @module languageMapper
+ * @category Utility
+ */
+
+ const hasIntlDisplayNames = Intl && typeof Intl === "object" && typeof Intl.DisplayNames === "function";
 
 var mappingExistsCache = {};
+/**
+ * Function to get language name based on ISO language code. Caches returned values by localeCode
+ * @param {string} languageCode 2-letter ISO 639-1 or 3-letter ISO 639-2/T or 3-letter language code, might have optional sub-tags for Script and Region that won't be used. See getLanguageDetails
+ * @param {string} [localeCode='en'] locale/language code to localize language name into
+ * @returns {(string|undefined)} localized language name or undefined if there is no localization available
+ */
 module.exports.getLanguageName = (languageCode, localeCode = 'en') => {
   if (mappingExistsCache[languageCode] && mappingExistsCache[languageCode][localeCode]) {
     return mappingExistsCache[languageCode][localeCode];
@@ -36,7 +48,27 @@ module.exports.getLanguageName = (languageCode, localeCode = 'en') => {
   return languageName;
 };
 
-module.exports.getLanguageDetails = function (languageCode, localeCode = 'en') {
+/**
+ * @typedef {Object} LanguageDetails
+ * @property {string} languageCode Initial normalized 2-letter ISO 639-1 or 3-letter ISO 639-2/T or 3-letter ISO 639-3 language code (without Script or Region sub-tags)
+ * @property {string} languageName Language name localized or in English
+ * @property {(string|undefined)} languageScript Script name based on 4-letter ISO-15924 script sub-tag (i.e. Cyrillic, Arabic, Latin...)
+ * @property {(string|undefined)} languageRegion Region name based on 2-letter ISO 3166-1 region sub-tag (i.e. US, GB...)
+ * @property {boolean} localized Are languageName, languageScript, languageRegion localized
+ * @property {string} type ISO 639-3 language type (i.e. 'living', 'historical', 'constructed')
+ * @property {string} scope ISO 639-3 language scope (i.e. 'individual', 'macrolanguage') See https://en.wikipedia.org/wiki/ISO_639:a
+ * @property {(string|undefined)} iso6393 ISO 693-3 3-letter language code
+ * @property {(string|undefined)} iso6392B ISO 693-2/B 3-letter language code
+ * @property {(string|undefined)} iso6392T ISO 693-2/T 3-letter language code
+ * @property {(string|undefined)} iso6391 ISO 693-1 2-letter language code
+ */
+/**
+ * Function to get various language details
+ * @param {string} languageCode Language identifier is a combination of sub-tags (Language[-Script][-Region]) for Language and optionally Script, and/or Region, according to BCP 47 and RFC 4647
+ * @param {string} [localeCode='en'] Locale/Language code to localize language information into
+ * @returns {LanguageDetails} details Language details 
+ */
+ module.exports.getLanguageDetails = function (languageCode, localeCode = 'en') {
 
   var [normalizedCode, scriptCode, regionCode] = languageCode.split('-');
   if (scriptCode && scriptCode.length < 4) { // if a only a regionCode
@@ -48,11 +80,13 @@ module.exports.getLanguageDetails = function (languageCode, localeCode = 'en') {
 
   var languageName;
   var localized = false;
+
   // Try to get localized name through standard Internationalization API
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DisplayNames/of
   if (hasIntlDisplayNames) {
     languageName = (new Intl.DisplayNames(localeCode, { type: 'language', fallback: 'none' })).of(normalizedCode);
   }
+
   if (!languageName) { // fallback to ISO-693.3 name
     languageName = details.name;
   } else {
@@ -80,6 +114,7 @@ module.exports.getLanguageDetails = function (languageCode, localeCode = 'en') {
   };
 };
 
+/** returns ISO 639 language code (2-letter if available or 3-letter otherwise) */
 module.exports.getLanguageCode = function(languageName) {
   var langs = getLangs();
 
