8713c02dbc5d3df56cf8cf656e1ec4664137247c
zhuiks <zhuke@ukr.net>
2021-07-02 19:25:12 -0500

    style fuzzySearch component

--- app/frontend/components/generic/fuzzy_search.js
+++ app/frontend/components/generic/fuzzy_search.js
@@ -20,12 +20,73 @@
 const { html } = require('../../helpers/ezra_helper.js');
 const Fuse = require('fuse.js');
 
+// animation inspired by https://codepen.io/takaneichinose/pen/ErGwPZ?editors=0100
 const template = html`
 <style>
+.search-box {
+  margin: auto 0 auto auto;
+  display: inline-block;
+  position: relative;
+  border-radius: 2.5em;
+  height: 2.5em;
+  background: var(--widget-bg-color, white);
+  z-index: 1;
+}
+input[type="search"] {
+  font-size: 1em;
+  color: var(--text-color, currentColor);
+  width: 2.5em;
+  height: 2.5em;
+  padding: 0.25em 1.2em;
+  border: solid 1px var(--accent-color, currentColor);
+  background: #0000;
+  z-index: 1;
+  box-sizing: border-box;
+  border-radius: 2em;
+  transition: width 800ms cubic-bezier(0.68, -0.55, 0.27, 1.55);
+  -webkit-appearance: none;
+}
+input[type="search"]::-webkit-search-decoration {
+	-webkit-appearance: none;
+}
+input[type="search"]:focus {
+  outline: none;
+}
+input[type="search"]:focus,
+input[type="search"]:not(:placeholder-shown) {
+  width: 15em;
+  padding-right: 2.25em;
+  padding-left: 0.75em;
+  transition: width 800ms cubic-bezier(0.68, -0.55, 0.27, 1.55);
+}
+
+button[type="reset"] {
+  font-size: 0.6em;
+  background-color: transparent;
+  width: 2em;
+  height: 2em;
+  border: 0;
+  padding: 0;
+  outline: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: calc(50% - 1em);
+  right: 1em;
+  z-index: -1;
+}
+button[type="reset"] svg {
+  width: 100%;
+  fill: var(--accent-color, currentColor);
+}
 </style>
-<div class="search-wrapper">
-  <input type="search">
-</div>
+<form class="search-box">
+  <input type="search" placeholder=" ">
+  <button type="reset">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!-- Font Awesome Free 5.15.3 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/></svg>
+  </button>
+</form>
 `;
 
 /**
@@ -58,7 +119,7 @@ class FuzzySearch extends HTMLElement {
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-    this.shadowRoot.querySelector('.search-wrapper input[type="search"]').addEventListener('keyup', (e) => this._handleKeyup(e));
+    this.shadowRoot.querySelector('.search-box input[type="search"]').addEventListener('input', (e) => this._handleInput(e));
   }
 
   async connectedCallback() {
@@ -83,7 +144,7 @@ class FuzzySearch extends HTMLElement {
     this._fuse = new Fuse(dataArr, { includeScore: true,  threshold: this.threshold, distance: this.distance, minMatchCharLength: 2, keys });
   }
 
-  _handleKeyup(event) {
+  _handleInput(event) {
     clearTimeout(this._searchTimeout);
     if (!this._fuse) {
       return;
