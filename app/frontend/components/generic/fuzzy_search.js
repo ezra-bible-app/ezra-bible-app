642186c5a448a4c6a6ecabcbbdfec80a5cadfce0
zhuiks <zhuke@ukr.net>
2021-07-02 19:59:54 -0500

    adjust fuzzySearch style

--- app/frontend/components/generic/fuzzy_search.js
+++ app/frontend/components/generic/fuzzy_search.js
@@ -20,6 +20,10 @@
 const { html } = require('../../helpers/ezra_helper.js');
 const Fuse = require('fuse.js');
 
+const ICON_SEARCH = `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!-- Font Awesome Free 5.15.3 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/></svg>
+`;
+
 // animation inspired by https://codepen.io/takaneichinose/pen/ErGwPZ?editors=0100
 const template = html`
 <style>
@@ -38,12 +42,12 @@ input[type="search"] {
   width: 2.5em;
   height: 2.5em;
   padding: 0.25em 1.2em;
-  border: solid 1px var(--accent-color, currentColor);
+  border: solid 1px var(--text-color, currentColor);
   background: #0000;
   z-index: 1;
   box-sizing: border-box;
   border-radius: 2em;
-  transition: width 800ms cubic-bezier(0.68, -0.55, 0.27, 1.55);
+  transition: width 800ms cubic-bezier(0.68, -0.55, 0.27, 1.55), border-radius 800ms ease-in;
   -webkit-appearance: none;
 }
 input[type="search"]::-webkit-search-decoration {
@@ -57,7 +61,7 @@ input[type="search"]:not(:placeholder-shown) {
   width: 15em;
   padding-right: 2.25em;
   padding-left: 0.75em;
-  transition: width 800ms cubic-bezier(0.68, -0.55, 0.27, 1.55);
+  border-radius: 5px;
 }
 
 button[type="reset"] {
@@ -78,14 +82,12 @@ button[type="reset"] {
 }
 button[type="reset"] svg {
   width: 100%;
-  fill: var(--accent-color, currentColor);
+  fill: var(--text-color, currentColor);
 }
 </style>
 <form class="search-box">
   <input type="search" placeholder=" ">
-  <button type="reset">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!-- Font Awesome Free 5.15.3 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/></svg>
-  </button>
+  <button type="reset">${ICON_SEARCH}</button>
 </form>
 `;
 
