f49b8ab7be12468124ba72f63cf335ea3ab86782
zhuiks <zhuke@ukr.net>
2021-08-05 18:34:38 -0500

    add info from iso-639-3 package

--- scripts/sword_languages_to_json.js
+++ scripts/sword_languages_to_json.js
@@ -1,11 +1,18 @@
 const fetchline = require('nodefetchline');
 const fs = require('fs');
+const iso6393 = require('iso-639-3');
+
 
 const SWORD_LANGUAGES_URL = "https://crosswire.org/svn/sword/trunk/locales.d/locales.conf";
 
 parseSwordLocales().then(languages => {
+  for(const code in languages) {
+    languages[code] = addIso639Details(code, languages[code]);
+  }
+
   fs.writeFileSync(`lib/languages.json`, JSON.stringify(languages, null, 2));
 });
+const indexedLangs = getIndexedLangs();
 
 
 /**
@@ -50,9 +57,9 @@ async function parseSwordLocales() {
     if (!isTextSection || line[0] == '#' || line.trim() == '') {
       continue;
     }
-    
+
     const data = parseLine(line.trim());
-    if(!data) {
+    if (!data) {
       continue;
     }
 
@@ -65,7 +72,7 @@ async function parseSwordLocales() {
 function parseLine(line) {
   const found = line.match(/^(?<code>[a-z0-9]{2,})(?:-(?<script>[a-z]{4}))?(?:-(?<region>[a-z]{2}))?(?:\.(?<locale>[a-z]{2,3}))?=(?<name>.+)$/i);
   if (!found) {
-    console.log(`Skipping line: ${line}`);
+    console.log(`Skipping line: "${line}"`);
     return undefined;
   }
   return {
@@ -77,16 +84,16 @@ function parseLine(line) {
   };
 }
 
-function addDataToMap(langObj, name, script=undefined, locale=undefined, region=undefined) {
+function addDataToMap(langObj, name, script = undefined, locale = undefined, region = undefined) {
   var newLangObj;
   const nameObj = locale ? { [locale]: name } : { 'name': name };
   if (script) {
-    newLangObj = { 
+    newLangObj = {
       ...langObj,
-      scripts: { 
+      scripts: {
         ...langObj.scripts,
-        [script]: langObj.scripts ? { ...langObj.scripts[script], ...nameObj } : nameObj 
-      } 
+        [script]: langObj.scripts ? { ...langObj.scripts[script], ...nameObj } : nameObj
+      }
     };
   } else if (region) { // region will be skipped if script is present. So far SWORD data has either script or region present
     newLangObj = {
@@ -97,11 +104,37 @@ function addDataToMap(langObj, name, script=undefined, locale=undefined, region=
       }
     };
   } else {
-    newLangObj = {...langObj, ...nameObj};
-  }  
+    newLangObj = { ...langObj, ...nameObj };
+  }
 
   return newLangObj;
 }
+
+function addIso639Details(code, data) {
+  if(indexedLangs[code]) {
+    return {
+      ...indexedLangs[code],
+      ...data,
+    };
+  }
+  console.log(`Couldn't find "${code}" in iso639-3 package`);
+  return data;
+}
+
+function getIndexedLangs() {
+  var indexedLangs = {};
+
+  for (const currentLang of iso6393) {
+    if (currentLang.iso6391) {
+      indexedLangs[currentLang.iso6391] = currentLang;
+    } else if (currentLang.iso6393) {
+      indexedLangs[currentLang.iso6393] = currentLang;
+    }
+  }
+
+  return indexedLangs;
+}
+
 
 // export functions for unit testing
 module.exports = {
