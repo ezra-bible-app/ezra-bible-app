44d288e2e1c2da183ff5cd20f177eca94283f39d
Tobias Klein <contact@tklein.info>
2021-10-31 09:29:00 +0100

    Optimize build script for win32

--- build_scripts/win32/build_win32.bat
+++ build_scripts/win32/build_win32.bat
@@ -1,4 +1,6 @@
 call npm install --arch=ia32
 call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -o node-sword-interface -v 13.2.3
 call copy node_modules\node-sword-interface\build\sword-build-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
+call npm config set python C:\Python27\python.exe
+call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -o sqlite3 -v 13.2.3
 call npm run commit-info
