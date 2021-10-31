b19afe345d33a0aba282b93de51204ee6041d0e9
Tobias Klein <contact@tklein.info>
2021-10-31 11:23:36 +0100

    Further optimize windows build script

--- build_scripts/win32/build_win32.bat
+++ build_scripts/win32/build_win32.bat
@@ -1,10 +1,12 @@
-echo Changing NPM configuration to use Python 2.7
+echo *** Changing NPM configuration to use Python 2.7 ***
 call npm config set python python2.7
 call npm config ls
-echo Building sqlite3
-call npm install sqlite3@5.0.0 --arch=ia32 --build-from-source --runtime=electron --target=13.2.3 --dist-url=https://electronjs.org/headers
-echo Installing all other dependencies
+echo *** Building sqlite3 ***
+call npm install sqlite3@5.0.0 --build-from-source --runtime=electron --target=13.2.3 --dist-url=https://electronjs.org/headers --target_arch=ia32
+echo *** Installing all other dependencies ***
 call npm install
+echo *** Rebuilding node-sword-interface ***
 call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -o node-sword-interface -v 13.2.3
 call copy node_modules\node-sword-interface\build\sword-build-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
+echo *** Generating commit info ***
 call npm run commit-info
