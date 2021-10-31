2b33d0223ae6ed5225591f87cb16d107474cb72c
Tobias Klein <contact@tklein.info>
2021-10-31 09:50:03 +0100

    Windows build: Continue to use sqlite3 4.2.0

--- build_scripts/win32/build_win32.bat
+++ build_scripts/win32/build_win32.bat
@@ -1,6 +1,7 @@
 call npm install --arch=ia32
 call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -o node-sword-interface -v 13.2.3
 call copy node_modules\node-sword-interface\build\sword-build-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
+call npm install sqlite3@4.2.0 --arch=ia32 --build-from-source --python=C:\\Python27\\python.exe
 call npm config set python C:\Python27\python.exe
 call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -o sqlite3 -v 13.2.3
 call npm run commit-info
