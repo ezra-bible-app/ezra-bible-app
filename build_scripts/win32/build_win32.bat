157b90b01af82bb74699b4883b72f60e15c34f53
Tobias Klein <contact@tklein.info>
2021-10-31 11:14:35 +0100

    Adjust windows build config

--- build_scripts/win32/build_win32.bat
+++ build_scripts/win32/build_win32.bat
@@ -1,6 +1,6 @@
-set PYTHON=python2.7
+call npm config set python python2.7
 call npm config ls
-call npm install sqlite3@5.0.0 --python=C:\Python27\python.exe --arch=ia32 --build-from-source --runtime=electron --target=13.2.3 --dist-url=https://electronjs.org/headers
+call npm install sqlite3@5.0.0 --arch=ia32 --build-from-source --runtime=electron --target=13.2.3 --dist-url=https://electronjs.org/headers
 call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -o node-sword-interface -v 13.2.3
 call copy node_modules\node-sword-interface\build\sword-build-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
 call npm run commit-info
