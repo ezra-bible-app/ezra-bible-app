ba1895fea637f8871e158578e4c9cc0d600c71e5
Tobias Klein <contact@tklein.info>
2021-10-31 08:47:43 +0100

    Separate windows build into build step and installer step

--- build_scripts/win32/build_win32.bat
+++ build_scripts/win32/build_win32.bat
@@ -1,10 +1,4 @@
 call npm install --arch=ia32
-call npm install sqlite3@4.2.0 --arch=ia32
-call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -w node-sword-interface -v 13.2.3
+call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -o node-sword-interface -v 13.2.3
 call copy node_modules\node-sword-interface\build\sword-build-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
 call npm run commit-info
-call npm run install-node-prune-win
-call npm run prune-node-modules
-call npm run package-win
-call npm run fix-binary-timestamps
-call npm run installer-win
