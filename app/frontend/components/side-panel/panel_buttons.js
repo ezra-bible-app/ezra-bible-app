f5f8a48cf6113019d99bba6f468c2808674b1942
zhuiks <zhuke@ukr.net>
2021-11-18 16:24:43 -0600

    panel-buttons / add state and emit events

--- app/frontend/components/side-panel/panel_buttons.js
+++ app/frontend/components/side-panel/panel_buttons.js
@@ -16,7 +16,8 @@
    along with Ezra Bible App. See the file LICENSE.
    If not, see <http://www.gnu.org/licenses/>. */
 
-const { html } = require('../../helpers/ezra_helper.js');
+const { html, waitUntilIdle } = require('../../helpers/ezra_helper.js');
+const eventController = require('../../controllers/event_controller.js');
 
 const template = html`
    <style>
@@ -31,14 +32,23 @@ const template = html`
       margin-inline-end: 0.5em;
     }
     
-    #panel-switches button {
-      font-size: var(--button-switch-size);
+    ::slotted(button) {
+      font-size: var(--button-switch-size) !important; /* There is an issue with ::slotted specificity: https://github.com/w3c/csswg-drafts/issues/6466 */
       color: var(--accent-color);
       background: none;
       padding: 6px;
       border: var(--area-border);
       border-radius: var(--border-radius);
       margin-block-end: 0.5em;
+      box-shadow: 1px 1px 6px #0002;
+    }
+
+    ::slotted(button.active) {
+      border-color: #aed0ea;
+      border-width: 2px;
+      background: #e3f1fa;
+      box-shadow: none;
+      transform: translateX(1px);
     }
    </style>
     
@@ -50,13 +60,63 @@ const template = html`
 class PanelButtons extends HTMLElement {
   constructor() {
     super();
+
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.panelStates = {};
   }
 
   connectedCallback() {
+    const slottedElements = this.shadowRoot.querySelector('slot').assignedElements();
+    slottedElements.forEach(el => this.initButton(el));
+  }
+
+  async initButton(buttonElement) {
+    const settingsKey = buttonElement.getAttribute('settings-key');
+    const defaultOpen = (buttonElement.getAttribute('default') == "true");
+    const isOpen = await ipcSettings.get(settingsKey, defaultOpen);
+    await this.updatePanel(buttonElement, isOpen, false);
+
+    buttonElement.addEventListener('click', async (e) => {
+      e.preventDefault();
+      await this.updatePanel(buttonElement, this.panelStates[settingsKey]);
+    });
+  }
+  
+  /**
+   * @param {HTMLElement} buttonElement 
+   * @param {boolean} isOpen 
+   */
+  async updatePanel(buttonElement, isOpen, saveSettings=true) {
+    if (!buttonElement.hasAttribute('settings-key')) {
+      console.log('Attribute "settings-key" is required for panel buttons!');
+      return;
+    }
+    const settingsKey = buttonElement.getAttribute('settings-key');
+
+    if (!buttonElement.hasAttribute('event')) {
+      console.log('Attribute "event" is required for panel buttons!');
+      return;
+    }
+    const emitEvent = buttonElement.getAttribute('event');
+
+    this.panelStates[settingsKey] = isOpen;
+    if (isOpen) {
+      buttonElement.classList.add('active');
+    } else {
+      buttonElement.classList.remove('active');
+    }
+
+    if (saveSettings) {
+      await waitUntilIdle();
+      await ipcSettings.set(settingsKey, isOpen);
+    }
+
+    eventController.publishAsync(emitEvent, isOpen);
   }
 }
 
 customElements.define('panel-buttons', PanelButtons);
 module.exports = PanelButtons;
+
