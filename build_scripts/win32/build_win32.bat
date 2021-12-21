b962708ca71d5f4f8e1b6ba8983e1d0defb702c8
Tobias Klein <contact@tklein.info>
2021-12-21 10:10:34 +0100

    Check error level when generating Windows build

--- build_scripts/win32/build_win32.bat
+++ build_scripts/win32/build_win32.bat
@@ -3,8 +3,10 @@ call npm config set python python2.7
 call npm config ls
 echo *** Building sqlite3 ***
 call npm install sqlite3@5.0.0 --build-from-source --runtime=electron --target=13.2.3 --dist-url=https://electronjs.org/headers --target_arch=ia32
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
 echo *** Installing all other dependencies ***
 call npm install --arch=ia32
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
 echo *** Rebuilding node-sword-interface ***
 call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -o node-sword-interface -v 13.2.3
 call copy node_modules\node-sword-interface\build\sword-build-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
