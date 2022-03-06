b4eb9ae07904867fc5b961a8a9980f6ba1330ef1
Tobias Klein <contact@tklein.info>
2022-03-06 16:28:04 +0100

    Tag.getAllTags / Do not consider tag groups for statistics

--- app/backend/database/models/tag.js
+++ app/backend/database/models/tag.js
@@ -140,16 +140,22 @@ module.exports = (sequelize, DataTypes) => {
                  " SUM(CASE WHEN vt.tagId IS NULL THEN 0 ELSE 1 END) AS globalAssignmentCount," +
                  " SUM(CASE WHEN vr.bibleBookId=" + bibleBookId + " THEN 1 ELSE 0 END) AS bookAssignmentCount";
 
-    query += ", GROUP_CONCAT(DISTINCT tg.id) AS tagGroupList";
+    if (!onlyStats) {
+      query += ", GROUP_CONCAT(DISTINCT tg.id) AS tagGroupList";
+    }
 
     query += ", strftime('%s', MAX(vt.updatedAt)) AS lastUsed";
 
     query += " FROM Tags t" +
              " LEFT JOIN VerseTags vt ON vt.tagId = t.id" +
-             " LEFT JOIN VerseReferences vr ON vt.verseReferenceId = vr.id" +
-             " LEFT JOIN TagGroupMembers tgm ON tgm.tagId = t.id" +
-             " LEFT JOIN TagGroups tg ON tg.id = tgm.tagGroupId" +
-             " GROUP BY t.id";
+             " LEFT JOIN VerseReferences vr ON vt.verseReferenceId = vr.id";
+
+    if (!onlyStats) {
+      query += " LEFT JOIN TagGroupMembers tgm ON tgm.tagId = t.id" +
+               " LEFT JOIN TagGroups tg ON tg.id = tgm.tagGroupId";
+    }
+
+    query += " GROUP BY t.id";
     
     if (lastUsed) {
       query += " ORDER BY lastUsed DESC limit 5";
