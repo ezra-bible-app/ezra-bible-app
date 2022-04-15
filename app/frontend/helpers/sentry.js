0ae88c7f02eded3fe840832cd6efdd6464e42770
Tobias Klein <contact@tklein.info>
2022-04-15 15:00:02 +0200

    Adjust require statements for Sentry integration

--- app/frontend/helpers/sentry.js
+++ app/frontend/helpers/sentry.js
@@ -1,4 +1,4 @@
-window.Sentry = require('@sentry/electron/dist/renderer');
+window.Sentry = require('@sentry/electron/renderer');
 
 Sentry.init({
   dsn: 'https://977e321b83ec4e47b7d28ffcbdf0c6a1@sentry.io/1488321',
