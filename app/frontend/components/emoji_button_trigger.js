37348c6f07a0476fa8f72dd1a4d28f82dab19aa7
Tobias Klein <contact@tklein.info>
2022-05-08 11:34:50 +0200

    Optimize emoji button trigger style

--- app/frontend/components/emoji_button_trigger.js
+++ app/frontend/components/emoji_button_trigger.js
@@ -29,7 +29,7 @@ const template = html`
       right: 0;
       bottom: auto;
       width: 1em;
-      margin-right: 1.3em;
+      margin-right: 1.8em;
       padding: 3px;
       fill: #5f5f5f;
       display: inline-block;
