0114afdf6bcb503120b69dec35904b27034bc737
Tobias Klein <contact@tklein.info>
2022-08-30 22:35:44 +0200

    Remove code related to Dropbox sync that is not needed anymore

--- app/frontend/controllers/db_sync_controller.js
+++ app/frontend/controllers/db_sync_controller.js
@@ -16,8 +16,6 @@
    along with Ezra Bible App. See the file LICENSE.
    If not, see <http://www.gnu.org/licenses/>. */
 
-const UiHelper = require('../helpers/ui_helper.js');
-const uiHelper = new UiHelper();
 const DROPBOX_TOKEN_SETTINGS_KEY = 'dropboxToken';
 const Dropbox = require('dropbox');
 const PlatformHelper = require('../../lib/platform_helper.js');
@@ -28,44 +26,6 @@ const platformHelper = new PlatformHelper();
  * @module dbSyncController
  * @category Controller
  */
-
-module.exports.showDbSyncConfigDialog = async function() {
-  var dialogWidth = 450;
-  var dialogHeight = 400;
-  var draggable = true;
-  var position = [55, 120];
-
-  let dbSyncDialogOptions = uiHelper.getDialogOptions(dialogWidth, dialogHeight, draggable, position);
-  dbSyncDialogOptions.title = i18n.t("general.setup-db-sync");
-  dbSyncDialogOptions.buttons = {};
-
-  let dropboxTokenValue = await ipcSettings.get(DROPBOX_TOKEN_SETTINGS_KEY, "");
-  $('#dropbox-token').val(dropboxTokenValue);
-
-  dbSyncDialogOptions.buttons[i18n.t("general.save")] = {
-    id: 'save-db-sync-config-button',
-    text: i18n.t("general.save"),
-    click: () => {
-      $('#db-sync-box').dialog("close");
-      this.saveDbSyncConfiguration();
-    }
-  };
-
-  dbSyncDialogOptions.buttons[i18n.t("general.cancel")] = {
-    id: 'cancel-db-sync-config-button',
-    text: i18n.t("general.cancel"),
-    click: () => {
-      $('#db-sync-box').dialog("close");
-    }
-  };
-
-  $('#db-sync-box').dialog(dbSyncDialogOptions);
-};
-
-module.exports.saveDbSyncConfiguration = async function() {
-  let dropboxTokenValue = $('#dropbox-token').val().trim();
-  await ipcSettings.set(DROPBOX_TOKEN_SETTINGS_KEY, dropboxTokenValue);
-};
 
 // Parses the url and gets the access token if it is in the urls hash
 module.exports.getCodeFromUrl = function(url) {
