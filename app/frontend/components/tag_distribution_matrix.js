a179909564e9e0e25b72ca19c8e66450cf4f79b6
Tobias Klein <contact@tklein.info>
2022-11-02 10:30:18 +0100

    TagDistributionMatrix / Implement copy to clipboard functionality

--- app/frontend/components/tag_distribution_matrix.js
+++ app/frontend/components/tag_distribution_matrix.js
@@ -16,10 +16,16 @@
    along with Ezra Bible App. See the file LICENSE.
    If not, see <http://www.gnu.org/licenses/>. */
 
-const { html } = require('../helpers/ezra_helper.js');
+const { html, getPlatform } = require('../helpers/ezra_helper.js');
 const verseListController = require('../controllers/verse_list_controller.js');
+const eventController = require('../controllers/event_controller.js');
+const UiHelper = require('../helpers/ui_helper.js');
 
 const template = html`
+
+<!-- JQUERY STYLES -->
+<link id="theme-css" href="css/jquery-ui/cupertino/jquery-ui.css" media="screen" rel="stylesheet" type="text/css" />
+
 <style>
   table {
     table-layout: fixed;
@@ -53,27 +59,69 @@ const template = html`
   }
 </style>
 
-<table style="margin-left: 1em; margin-top: 1em; margin-bottom: 1em; border-spacing: 0; border-collapse: collapse;">
-  <thead>
-    <tr id="header-row">
-    </tr>
-  </thead>
-  <tbody id="matrix-tbody">
-  </tbody>
-</table>
+<div id="tag-distribution-matrix" style="margin-bottom: 1em;">
+  <div id="table-wrapper">
+    <table style="margin-left: 1em; margin-top: 1em; margin-bottom: 1em; border-spacing: 0; border-collapse: collapse;">
+      <thead>
+        <tr id="header-row">
+        </tr>
+      </thead>
+      <tbody id="matrix-tbody">
+      </tbody>
+    </table>
+  </div>
+
+  <button id="copy-table-to-clipboard"
+          class="fg-button ui-corner-all ui-state-default"
+          style="margin-left: 1em; display: none;"
+          i18n="bible-browser.copy-table-to-clipboard"></button>
+</div>
 `;
 
 class TagDistributionMatrix extends HTMLElement {
   constructor() {
     super();
 
-    this._input = null;
+    this._uiHelper = new UiHelper();
 
-    this._shadowRoot = this.attachShadow({ mode: 'open' });
-    this._shadowRoot.appendChild(template.content.cloneNode(true));
+    this._input = null;
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.localize();
+
+    eventController.subscribe('on-locale-changed', () => {
+      this.localize();
+    });
+  }
+
+  localize() {
+    let mainElement = this.shadowRoot.getElementById('tag-distribution-matrix');
+    $(mainElement).localize();
   }
 
   connectedCallback() {
+    this.shadowRoot.getElementById('copy-table-to-clipboard').addEventListener('click', () => {
+      const tableWrapper = this.shadowRoot.getElementById('table-wrapper');
+      const tableHtml = tableWrapper.innerHTML;
+      const platform = getPlatform();
+      platform.copyHtmlToClipboard(tableHtml);
+
+      // eslint-disable-next-line no-undef
+      iziToast.success({
+        message: i18n.t('bible-browser.copy-table-to-clipboard-success'),
+        position: 'bottomRight',
+        timeout: 3000
+      });
+    });
+
+    eventController.subscribe('on-theme-changed', (theme) => {
+      if (theme == 'dark') {
+        this._uiHelper.switchToDarkTheme(this.shadowRoot, 'tag-distribution-matrix');
+      } else {
+        this._uiHelper.switchToRegularTheme(this.shadowRoot, 'tag-distribution-matrix');
+      }
+    });
   }
 
   set input(value) {
@@ -87,11 +135,13 @@ class TagDistributionMatrix extends HTMLElement {
   }
 
   reset() {
-    let headerRow = this._shadowRoot.getElementById('header-row');
+    let headerRow = this.shadowRoot.getElementById('header-row');
     headerRow.innerHTML = '';
 
-    const matrixTBody = this._shadowRoot.getElementById('matrix-tbody');
+    const matrixTBody = this.shadowRoot.getElementById('matrix-tbody');
     matrixTBody.innerHTML = '';
+
+    this.shadowRoot.getElementById('copy-table-to-clipboard').style.display = 'none';
   }
 
   async refresh(tabIndex=undefined) {
@@ -99,7 +149,7 @@ class TagDistributionMatrix extends HTMLElement {
     const bibleBookStats = verseListController.getBibleBookStatsFromVerseList(tabIndex);
     const actualBooks = Object.keys(bibleBookStats);
 
-    let headerRow = this._shadowRoot.getElementById('header-row');
+    let headerRow = this.shadowRoot.getElementById('header-row');
     headerRow.innerHTML = '';
     let firstHeader = document.createElement('th');
     firstHeader.style.width = '15em';
@@ -111,7 +161,7 @@ class TagDistributionMatrix extends HTMLElement {
       headerRow.appendChild(bookHeader);
     });
 
-    const matrixTBody = this._shadowRoot.getElementById('matrix-tbody');
+    const matrixTBody = this.shadowRoot.getElementById('matrix-tbody');
     matrixTBody.innerHTML = '';
 
     for (let i = 0; i < tagIdList.length; i++) {
@@ -137,6 +187,10 @@ class TagDistributionMatrix extends HTMLElement {
       });
 
       matrixTBody.appendChild(currentTagRow);
+    }
+
+    if (platformHelper.isElectron()) {
+      this.shadowRoot.getElementById('copy-table-to-clipboard').style.removeProperty('display');
     }
   }
 
