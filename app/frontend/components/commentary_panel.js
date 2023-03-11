dc5c721e1aa31743f4b66a9fa8c2fab903eb6f58
Tobias Klein <contact@tklein.info>
2023-03-11 20:12:28 +0100

    Implement basic commentary panel functionality

---
+++ app/frontend/components/commentary_panel.js
@@ -0,0 +1,162 @@
+/* This file is part of Ezra Bible App.
+
+   Copyright (C) 2019 - 2023 Ezra Bible App Development Team <contact@ezrabibleapp.net>
+
+   Ezra Bible App is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   Ezra Bible App is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Ezra Bible App. See the file LICENSE.
+   If not, see <http://www.gnu.org/licenses/>. */
+
+const eventController = require('../controllers/event_controller.js');
+const VerseBox = require("../ui_models/verse_box.js");
+
+/**
+ * The CommentaryPanel component implements a tool panel that shows Bible commentaries for selected verses
+ * 
+ * @category Component
+ */
+class CommentaryPanel {
+  constructor() {
+    eventController.subscribe('on-verses-selected', (verseSelectionDetails) => {
+      this.performRefresh(verseSelectionDetails.selectedElements);
+    });
+
+    eventController.subscribe('on-commentary-panel-switched', () => {
+      let selectedVerseBoxes = app_controller.verse_selection.getSelectedElements();
+      this.performRefresh(selectedVerseBoxes);
+    });
+
+    eventController.subscribe('on-locale-changed', () => {
+      let selectedVerseBoxes = app_controller.verse_selection.getSelectedElements();
+      this.performRefresh(selectedVerseBoxes);
+    });
+  }
+
+  getBoxContent() {
+    return document.getElementById('commentary-panel-content');
+  }
+
+  showLoadingIndicator() {
+    var loadingIndicator = document.getElementById('commentary-panel-loading-indicator');
+    loadingIndicator.querySelector('.loader').style.display = 'block';
+    loadingIndicator.style.display = 'block';
+  }
+  
+  hideLoadingIndicator() {
+    var loadingIndicator = document.getElementById('commentary-panel-loading-indicator');
+    loadingIndicator.style.display = 'none';
+  }
+
+  isPanelActive() {
+    var panelButtons = document.getElementById('panel-buttons');
+    return panelButtons.activePanel == 'commentary-panel';
+  }
+
+  getHelpBox() {
+    return document.getElementById('commentary-panel-help');
+  }
+
+  async performRefresh(selectedVerseBoxes) {
+    if (!this.isPanelActive()) {
+      return;
+    }
+
+    let allCommentaries = await ipcNsi.getAllLocalModules('COMMENTARY');
+    if (allCommentaries.length == 0) {
+      return;
+    }
+
+    let panelHeader = document.getElementById('commentary-panel-header');
+    let helpBox = this.getHelpBox();
+    let panelTitle = "";
+
+    if (app_controller.verse_selection != null &&
+        app_controller.verse_selection.selected_verse_box_elements != null &&
+        app_controller.verse_selection.selected_verse_box_elements.length > 0) {
+
+      panelTitle = i18n.t("commentary-panel.commentaries-for") + " " + 
+        await app_controller.verse_selection.getSelectedVerseLabelText();
+
+      helpBox.classList.add('hidden');
+
+    } else {
+      panelTitle = i18n.t("commentary-panel.default-header");
+      helpBox.classList.remove('hidden');
+    }
+
+    panelHeader.innerHTML = "<b>" + panelTitle + "</b>";
+
+    if (platformHelper.isCordova()) {
+
+      this.getBoxContent().innerHTML = "";
+      this.showLoadingIndicator();
+      this.performDelayedContentRefresh();
+
+    } else {
+
+      await this.performContentRefresh(selectedVerseBoxes);
+    }
+  }
+
+  async performContentRefresh(selectedVerseBoxes=undefined) {
+    var commentaryContent = await this.getCommentaryContent(selectedVerseBoxes);
+
+    if (platformHelper.isCordova()) {
+      this.hideLoadingIndicator();
+    }
+
+    this.getBoxContent().innerHTML = commentaryContent;
+  }
+
+  performDelayedContentRefresh() {
+    setTimeout(async () => {
+      await this.performContentRefresh();
+    }, 50);
+  }
+
+  async getCommentaryContent(selectedVerseBoxes=undefined) {
+    let commentaryContent = "";
+
+    if (selectedVerseBoxes != null) {
+      if (selectedVerseBoxes.length != 0) {
+        let allCommentaries = await ipcNsi.getAllLocalModules('COMMENTARY');
+
+        for (let i = 0; i < allCommentaries.length; i++) {
+          let currentCommentary = allCommentaries[i];
+
+          for (let j = 0; j < selectedVerseBoxes.length; j++) {
+            let currentVerseBox = $(selectedVerseBoxes[j]);
+
+            let verseCommentary = await this.getCommentaryForVerse(currentCommentary.name, currentVerseBox);
+
+            if (verseCommentary.length != 0) {
+              commentaryContent += `<h3>${currentCommentary.description}</h3>`;
+              commentaryContent += verseCommentary;
+            }
+          }
+        }
+      }
+    }
+
+    return commentaryContent;
+  }
+
+  async getCommentaryForVerse(commentaryId, verseBox) {
+    let referenceVerseBox = new VerseBox(verseBox[0]);
+    let bibleBookShortTitle = referenceVerseBox.getBibleBookShortTitle();
+    let reference = bibleBookShortTitle + ' ' + referenceVerseBox.getChapter() + ':' + referenceVerseBox.getVerseNumber();
+    let commentaryEntry = await ipcNsi.getReferenceText(commentaryId, reference);
+    return commentaryEntry.content;
+  }
+}
+
+module.exports = CommentaryPanel;
