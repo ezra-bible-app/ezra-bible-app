0f2d9957ca02b5e1351a09013054f5ae55a957b6
Tobias Klein <contact@tklein.info>
2023-10-28 17:34:26 +0200

    Try with sqlite3 5.1.6

--- build_scripts/win32/build_win32.bat
+++ build_scripts/win32/build_win32.bat
@@ -1,8 +1,8 @@
-echo *** Changing NPM configuration to use Python 2.7 ***
-call npm config set python python2.7
+REM echo *** Changing NPM configuration to use Python 2.7 ***
+REM call npm config set python python2.7
 call npm config ls
 echo *** Building sqlite3 ***
-call npm install sqlite3@5.0.0 --build-from-source --runtime=electron --target=17.1.0 --dist-url=https://electronjs.org/headers --target_arch=ia32
+call npm install sqlite3@5.1.6 --build-from-source --runtime=electron --target=17.1.0 --dist-url=https://electronjs.org/headers --target_arch=ia32
 if %ERRORLEVEL% GEQ 1 EXIT /B 1
 echo *** Installing all other dependencies ***
 call npm install --arch=ia32
