fc3452fcdcab0d2fafe98c04db0ad9e446add3a6
Tobias Klein <contact@tklein.info>
2023-11-18 10:33:46 +0100

    Upgrade @electron/notarize to 2.2.0 and @electron/osx-sign to 1.0.5

--- build_scripts/macos/notarize_mac.sh
+++ build_scripts/macos/notarize_mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Enter your Apple ID"
+echo "Enter your Apple ID:"
 read APPLE_ID
 
 echo ""
@@ -8,9 +8,14 @@ echo "Enter your Apple ID password:"
 read APPLE_ID_PW
 
 echo ""
+echo "Enter your Apple Team ID (get this from https://developer.apple.com/account):"
+read APPLE_TEAM_ID
+
+echo ""
 
 export APPLE_ID="${APPLE_ID}"
 export APPLE_ID_PW="${APPLE_ID_PW}"
+export APPLE_TEAM_ID="${APPLE_TEAM_ID}"
 
 echo "Notarizing app ..."
 node build_scripts/macos/notarize_mac.js
