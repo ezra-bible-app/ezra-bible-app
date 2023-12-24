42622a27811846ef2bb6e7562a99ca1143ee32d7
Tobias Klein <contact@tklein.info>
2023-12-24 10:15:58 +0100

    SectionLabelHelper / Adjust code style

--- app/frontend/helpers/section_label_helper.js
+++ app/frontend/helpers/section_label_helper.js
@@ -25,50 +25,50 @@ module.exports.initHelper = function(nsi) {
 };
 
 module.exports.getSelectedVerseDisplayText = async function(selectedBooks, selectedVerseBoxElements) {
-  var selected_verses_content = [];
+  let selectedVerseContent = [];
 
-  for (var i = 0; i < selectedBooks.length; i++) {
-    var currentBookShortName = selectedBooks[i];
-    var currentBookVerseReferences = [];
+  for (let i = 0; i < selectedBooks.length; i++) {
+    let currentBookShortName = selectedBooks[i];
+    let currentBookVerseReferences = [];
     
-    for (var j = 0; j < selectedVerseBoxElements.length; j++) {
-      var currentVerseBox = selectedVerseBoxElements[j];
+    for (let j = 0; j < selectedVerseBoxElements.length; j++) {
+      let currentVerseBox = selectedVerseBoxElements[j];
 
-      var currentVerseBibleBookShortName = new VerseBox(currentVerseBox).getBibleBookShortTitle();
+      let currentVerseBibleBookShortName = new VerseBox(currentVerseBox).getBibleBookShortTitle();
 
       if (currentVerseBibleBookShortName == currentBookShortName) {
-        var currentVerseReference = this.getVerseReferenceFromAnchor($(currentVerseBox).find('a:first').attr('name'));
+        let currentVerseReference = this.getVerseReferenceFromAnchor($(currentVerseBox).find('a:first').attr('name'));
         currentBookVerseReferences.push(currentVerseReference);
       }
     }
 
-    var formatted_verse_list = await this.format_verse_list_for_view(currentBookVerseReferences, false, currentBookShortName);
-    var currentBookName = await (currentBookShortName == 'Ps' ? i18nHelper.getPsalmTranslation() : ipcDb.getBookTitleTranslation(currentBookShortName));
-    var currentBookVerseReferenceDisplay = currentBookName + ' ' + formatted_verse_list;
-    selected_verses_content.push(currentBookVerseReferenceDisplay);
+    let formattedVerseList = await this.formatVerseList(currentBookVerseReferences, false, currentBookShortName);
+    let currentBookName = await (currentBookShortName == 'Ps' ? i18nHelper.getPsalmTranslation() : ipcDb.getBookTitleTranslation(currentBookShortName));
+    let currentBookVerseReferenceDisplay = currentBookName + ' ' + formattedVerseList;
+    selectedVerseContent.push(currentBookVerseReferenceDisplay);
   }
 
-  if (selected_verses_content.length > 0) {
-    return selected_verses_content.join('; ');
+  if (selectedVerseContent.length > 0) {
+    return selectedVerseContent.join('; ');
   } else {
     return i18n.t("tags.none-selected");
   }
 };
 
-module.exports.verse_list_has_gaps = function(list) {
-  var has_gaps = false;
+module.exports.verseListHasGaps = function(list) {
+  let hasGaps = false;
 
-  for (var i = 1; i < list.length; i++) {
+  for (let i = 1; i < list.length; i++) {
     if ((list[i] - list[i-1]) > 1) {
-      has_gaps = true;
+      hasGaps = true;
       break;
     }
   }
 
-  return has_gaps;
+  return hasGaps;
 };
 
-module.exports.format_single_verse_block = async function(list, start_index, end_index, turn_into_link, bookId=undefined) {
+module.exports.formatSingleVerseBlock = async function(list, start_index, end_index, turn_into_link, bookId=undefined) {
   if (bookId == undefined) {
     bookId = app_controller.tab_controller.getTab().getBook();
   }
@@ -76,16 +76,16 @@ module.exports.format_single_verse_block = async function(list, start_index, end
   if (start_index > (list.length - 1)) start_index = list.length - 1;
   if (end_index > (list.length - 1)) end_index = list.length - 1;
 
-  var start_reference = list[start_index];
-  var end_reference = list[end_index];
+  let start_reference = list[start_index];
+  let end_reference = list[end_index];
 
-  var formatted_passage = "";
+  let formatted_passage = "";
 
   if (start_reference != undefined && end_reference != undefined) {
-    formatted_passage = await this.format_passage_reference_for_view(bookId,
-                                                                     start_reference,
-                                                                     end_reference,
-                                                                     ':');
+    formatted_passage = await this.formatPassageReference(bookId,
+                                                          start_reference,
+                                                          end_reference,
+                                                          ':');
 
     /*if (turn_into_link) {
       formatted_passage = "<a href=\"javascript:app_controller.jumpToReference('" + start_reference + "', true);\">" + formatted_passage + "</a>";
@@ -95,7 +95,7 @@ module.exports.format_single_verse_block = async function(list, start_index, end
   return formatted_passage;
 };
 
-module.exports.verse_reference_list_to_absolute_verse_nr_list = async function(list, bookId=undefined) {
+module.exports.verseReferenceListToAbsoluteVerseNrList = async function(list, bookId=undefined) {
   if (this.verseReferenceHelper == null) {
     return [];
   }
@@ -115,72 +115,72 @@ module.exports.verse_reference_list_to_absolute_verse_nr_list = async function(l
   return new_list;
 };
 
-module.exports.format_verse_list_for_view = async function(selected_verse_array, link_references, bookId=undefined) {
-  var absolute_nr_list = await this.verse_reference_list_to_absolute_verse_nr_list(selected_verse_array, bookId);
-  var verse_list_for_view = "";
+module.exports.formatVerseList = async function(selected_verse_array, link_references, bookId=undefined) {
+  const absolute_nr_list = await this.verseReferenceListToAbsoluteVerseNrList(selected_verse_array, bookId);
+  let verse_list_for_view = "";
 
   if (selected_verse_array.length > 0) {
-    if (this.verse_list_has_gaps(absolute_nr_list)) {
-      var current_start_index = 0;
+    if (this.verseListHasGaps(absolute_nr_list)) {
+      let current_start_index = 0;
 
-      for (var i = 0; i < absolute_nr_list.length; i++) {
+      for (let i = 0; i < absolute_nr_list.length; i++) {
         if (absolute_nr_list[i] - absolute_nr_list[i-1] > 1) {
 
-          var current_end_index = i - 1;
+          let current_end_index = i - 1;
           
-          verse_list_for_view += await this.format_single_verse_block(selected_verse_array,
-                                                                      current_start_index,
-                                                                      current_end_index,
-                                                                      link_references,
-                                                                      bookId);
+          verse_list_for_view += await this.formatSingleVerseBlock(selected_verse_array,
+                                                                   current_start_index,
+                                                                   current_end_index,
+                                                                   link_references,
+                                                                   bookId);
 
           verse_list_for_view += "; ";
 
           if (i == (absolute_nr_list.length - 1)) {
-            verse_list_for_view += await this.format_single_verse_block(selected_verse_array,
-                                                                        i,
-                                                                        i,
-                                                                        link_references,
-                                                                        bookId);
+            verse_list_for_view += await this.formatSingleVerseBlock(selected_verse_array,
+                                                                     i,
+                                                                     i,
+                                                                     link_references,
+                                                                     bookId);
           }
 
           current_start_index = i;
         } else {
           if (i == (absolute_nr_list.length - 1)) {
-            verse_list_for_view += await this.format_single_verse_block(selected_verse_array,
-                                                                        current_start_index,
-                                                                        i,
-                                                                        link_references,
-                                                                        bookId);
+            verse_list_for_view += await this.formatSingleVerseBlock(selected_verse_array,
+                                                                     current_start_index,
+                                                                     i,
+                                                                     link_references,
+                                                                     bookId);
           }
         }
       }
     } else { // verse_list doesn't have gaps!
-      verse_list_for_view += await this.format_single_verse_block(selected_verse_array,
-                                                                  0,
-                                                                  selected_verse_array.length - 1,
-                                                                  link_references,
-                                                                  bookId);
+      verse_list_for_view += await this.formatSingleVerseBlock(selected_verse_array,
+                                                               0,
+                                                               selected_verse_array.length - 1,
+                                                               link_references,
+                                                               bookId);
     }
   }
 
   return verse_list_for_view;
 };
 
-module.exports.format_passage_reference_for_view = async function(book_short_title, start_reference, end_reference, reference_separator=undefined) {
-  var bibleTranslationId = app_controller.tab_controller.getTab().getBibleTranslationId();
+module.exports.formatPassageReference = async function(book_short_title, start_reference, end_reference, reference_separator=undefined) {
+  let bibleTranslationId = app_controller.tab_controller.getTab().getBibleTranslationId();
 
   if (reference_separator == null) {
     reference_separator = await i18nHelper.getReferenceSeparator(bibleTranslationId);
   }
 
-  var start_chapter = parseInt(start_reference.split(reference_separator)[0]);
-  var start_verse = parseInt(start_reference.split(reference_separator)[1]);
-  var end_chapter = parseInt(end_reference.split(reference_separator)[0]);
-  var end_verse = parseInt(end_reference.split(reference_separator)[1]);
+  let start_chapter = parseInt(start_reference.split(reference_separator)[0]);
+  let start_verse = parseInt(start_reference.split(reference_separator)[1]);
+  let end_chapter = parseInt(end_reference.split(reference_separator)[0]);
+  let end_verse = parseInt(end_reference.split(reference_separator)[1]);
 
-  var passage = start_chapter + window.reference_separator + start_verse;
-  var endChapterVerseCount = await ipcNsi.getChapterVerseCount(bibleTranslationId, book_short_title, end_chapter);
+  let passage = start_chapter + window.reference_separator + start_verse;
+  let endChapterVerseCount = await ipcNsi.getChapterVerseCount(bibleTranslationId, book_short_title, end_chapter);
 
   if (book_short_title != null &&
       start_verse == 1 &&
@@ -211,7 +211,7 @@ module.exports.format_passage_reference_for_view = async function(book_short_tit
 };
 
 module.exports.getVerseReferenceFromAnchor = function(anchorText) {
-  var splittedVerseReference = anchorText.split(" ");
-  var currentVerseReference = splittedVerseReference[splittedVerseReference.length - 1];
+  let splittedVerseReference = anchorText.split(" ");
+  let currentVerseReference = splittedVerseReference[splittedVerseReference.length - 1];
   return currentVerseReference;
 };
