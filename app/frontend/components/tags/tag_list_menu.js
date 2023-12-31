127d265a6bc3fe3d147b6c57e4a4ac5e3c7faafa
Tobias Klein <contact@tklein.info>
2023-12-31 15:09:51 +0100

    Also subscribe to tagGroupLinkEvent

--- app/frontend/components/tags/tag_list_menu.js
+++ app/frontend/components/tags/tag_list_menu.js
@@ -211,6 +211,17 @@ class TagListMenu extends HTMLElement {
       }
     });
 
+    eventController.subscribe(this._tagGroupLinkEvent, () => {
+      this.hideTagGroupDisplay();
+      this.getTagGroupListLink().classList.add('list-tag-groups');
+
+      if (this._addElementButtons) {
+        this.showAddTagGroupButton();
+      }
+
+      this.hideAddTagButton();
+    });
+
     this.localize();
 
     this.shadowRoot.getElementById('new-standard-tag-button').addEventListener('click', async function(event) {
@@ -229,14 +240,6 @@ class TagListMenu extends HTMLElement {
   }
 
   onTagGroupListLinkClicked() {
-    this.hideTagGroupDisplay();
-    this.getTagGroupListLink().classList.add('list-tag-groups');
-
-    if (this._addElementButtons) {
-      this.showAddTagGroupButton();
-    }
-
-    this.hideAddTagButton();
     eventController.publishAsync(this._tagGroupLinkEvent);
   }
 
