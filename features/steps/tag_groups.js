bb21ebd5cf48345c34f243a8d2acba55f6fa5f99
Tobias Klein <contact@tklein.info>
2024-01-01 13:43:14 +0100

    Wait longer when adding tags to a group

--- features/steps/tag_groups.js
+++ features/steps/tag_groups.js
@@ -137,7 +137,7 @@ Given('I select the tag {string} to be added to the current tag group', async fu
 When('I add the selected tags to the group', async function () {
   let addTagsToGroupButton = await spectronHelper.getWebClient().$('#add-tags-to-group-button');
   await addTagsToGroupButton.click();
-  await spectronHelper.sleep(1000);
+  await spectronHelper.sleep(2000);
 });
 
 Then('the following tags are assigned to the tag group {string}', async function (tagGroup, dataTable) {
