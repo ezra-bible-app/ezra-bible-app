b21a78a7c417d071c439e328528e13a726c234cf
Tobias Klein <contact@tklein.info>
2024-11-10 18:06:21 +0100

    Cleanup windows build file

--- build_scripts/win32/build_win32.bat
+++ build_scripts/win32/build_win32.bat
@@ -1,5 +1,3 @@
-REM echo *** Changing NPM configuration to use Python 2.7 ***
-REM call npm config set python python2.7
 call npm config ls
 echo *** Building sqlite3 ***
 call npm install sqlite3@5.1.6 --build-from-source --runtime=electron --target=33.2.0 --dist-url=https://electronjs.org/headers --target_arch=ia32
@@ -7,9 +5,6 @@ if %ERRORLEVEL% GEQ 1 EXIT /B 1
 echo *** Installing all other dependencies ***
 call npm install --arch=ia32
 if %ERRORLEVEL% GEQ 1 EXIT /B 1
-REM echo *** Rebuilding node-sword-interface ***
-REM call node_modules\.bin\electron-rebuild.cmd --arch=ia32 -f -o node-sword-interface -v 33.2.0
-REM call copy node_modules\node-sword-interface\build\sword-build-win32\lib\*.dll node_modules\node-sword-interface\build\Release\
 echo *** Generating commit info ***
 call npm run add-github-workspace-to-safe-git-dirs
 call npm run commit-info
