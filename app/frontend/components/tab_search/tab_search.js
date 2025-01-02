bdfdbced90206cddbba7e843edaf776370ecf300
Tobias Klein <contact@tklein.info>
2025-01-02 15:53:04 +0100

    Fix selector for verse text container in TabSearch component (fix #1181)

--- app/frontend/components/tab_search/tab_search.js
+++ app/frontend/components/tab_search/tab_search.js
@@ -275,7 +275,7 @@ class TabSearch {
     if (this.previousOccuranceElement != null) {
       this.previousOccuranceElement.classList.remove('current-hl');
       let closestVerseBox = this.previousOccuranceElement.closest('.verse-box');
-      if (closestVerseBox != null) closestVerseBox.querySelector('.verse-text').classList.remove('ui-selected');
+      if (closestVerseBox != null) closestVerseBox.querySelector('.verse-text-container').classList.remove('ui-selected');
       app_controller.verse_selection.clearVerseSelection(false);
     }
 
@@ -285,7 +285,7 @@ class TabSearch {
       let verseBox = this.currentOccuranceElement.closest('.verse-box');
 
       if (verseBox != null) {
-        verseBox.querySelector('.verse-text').classList.add('ui-selected');
+        verseBox.querySelector('.verse-text-container').classList.add('ui-selected');
         app_controller.verse_selection.updateSelected();
         app_controller.verse_selection.updateViewsAfterVerseSelection();
         await app_controller.navigation_pane.updateNavigationFromVerseBox(this.currentOccuranceElement, verseBox);
