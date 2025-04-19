a21a77da3fe30c64d430ae4a0ce3c5dacbe1a00f
Tobias Klein <contact@tklein.info>
2025-04-19 08:57:20 +0200

    Fix license header

--- app/frontend/components/options_menu/config_option.js
+++ app/frontend/components/options_menu/config_option.js
@@ -9,7 +9,8 @@
 
    Ezra Bible App is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+   See the GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
    along with Ezra Bible App. See the file LICENSE.
