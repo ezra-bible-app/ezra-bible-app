7fd9e35e8231c44189063f1bbcc9b52117525ed5
Tobias Klein <contact@tklein.info>
2025-05-15 13:17:07 +0200

    Enhance TagManager class with detailed documentation for constructor and getDbItems method

--- app/frontend/components/tags/tag_manager.js
+++ app/frontend/components/tags/tag_manager.js
@@ -9,8 +9,7 @@
 
    Ezra Bible App is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
    along with Ezra Bible App. See the file LICENSE.
@@ -21,8 +20,30 @@ const eventController = require('../../controllers/event_controller.js');
 
 /**
  * The TagManager is a special ItemListManager for a list of tags.
+ * 
+ * This class provides functionality to manage, display, and interact with tags in the application.
+ * It extends the ItemListManager class and specializes in tag-specific operations such as:
+ * - Handling tag selection and deselection
+ * - Responding to tag clicks
+ * - Filtering tags
+ * - Managing the list of selected tags
+ * - Updating the UI when tags are renamed or deleted
+ * 
+ * @class
+ * @extends ItemListManager
  */
 class TagManager extends ItemListManager {
+  /**
+   * Creates a new TagManager instance.
+   * 
+   * @param {Function} onClickHandler - Callback function that is invoked when a tag is clicked.
+   *                                    The function receives the click event and selection state as parameters.
+   * @param {boolean} selectable - Whether tags in this manager should be selectable. Defaults to false.
+   * @param {boolean} editable - Whether tags in this manager should be editable (show edit/delete buttons). Defaults to false.
+   * @param {string} cssClass - The CSS class that will be applied to each tag element in the list.
+   * @param {Array} virtualItems - An optional array of virtual tag items to include at the beginning of the list.
+   *                              Virtual items should have 'id' and 'title' properties. Defaults to empty array.
+   */
   constructor(onClickHandler,
               selectable=false,
               editable=false,
@@ -49,6 +70,16 @@ class TagManager extends ItemListManager {
     });
   }
 
+  /**
+   * Retrieves all tags from the database via the tag_store.
+   * 
+   * This method overrides the base getDbItems method from ItemListManager.
+   * It fetches the current list of tags from the tag_store and returns them.
+   * 
+   * @async
+   * @returns {Array} An array of tag objects, each containing at least 'id' and 'title' properties.
+   * @override
+   */
   async getDbItems() {
     let dbItems = await tag_assignment_panel.tag_store.getTagList();
     return dbItems;
