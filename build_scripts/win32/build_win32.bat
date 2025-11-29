4a17721434281b77a53c9f8d4a56feb4c0358f54
Tobias Klein <contact@tklein.info>
2025-11-29 08:48:41 +0100

    Fix Windows build (#1274)
    * Re-enable windows build
    * Use node-gyp 12.1.0
    * Use node 20.17.0
    * Use sqlite 5.1.7
    * Comment out sqlite3 build commands in Windows batch script
    * Update Sentry dependencies to latest versions
    * Remove spectron dependency from package.json
    * Update docx dependency to version 9.5.1
    * Downgrade @sentry/electron dependency to version 7.2.0
    * Fix Windows runner version in build workflow to 2022
    * Add installation step for spectron in CI workflow
    * Update node-sword-interface dependency to version 1.0.49

--- build_scripts/win32/build_win32.bat
+++ build_scripts/win32/build_win32.bat
@@ -1,8 +1,8 @@
 call npm config ls
-echo *** Building sqlite3 ***
-call npm install sqlite3@5.1.6 --build-from-source --runtime=electron --target=32.2.3 --dist-url=https://electronjs.org/headers --target_arch=ia32
-if %ERRORLEVEL% GEQ 1 EXIT /B 1
-echo *** Installing all other dependencies ***
+REM echo *** Building sqlite3 ***
+REM call npm install sqlite3@5.1.6 --build-from-source --runtime=electron --target=32.2.3 --dist-url=https://electronjs.org/headers --target_arch=ia32
+REM if %ERRORLEVEL% GEQ 1 EXIT /B 1
+REM echo *** Installing all other dependencies ***
 call npm install --arch=ia32
 if %ERRORLEVEL% GEQ 1 EXIT /B 1
 echo *** Generating commit info ***
