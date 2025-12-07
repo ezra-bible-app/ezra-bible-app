7e5315930381b5b810aa06a9699caca3af0d3b00
Tobias Klein <contact@tklein.info>
2025-12-07 16:01:20 +0100

    Comment out sqlite3 installation in macOS build script

--- build_scripts/macos/build_mac.sh
+++ build_scripts/macos/build_mac.sh
@@ -7,7 +7,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.10
 npm install
 npm run compile-pug
 npm run commit-info
-npm install sqlite3@5.0.2 --build-from-source --runtime=electron --target=32.2.3 --dist-url=https://electronjs.org/headers
+#npm install sqlite3@5.0.2 --build-from-source --runtime=electron --target=32.2.3 --dist-url=https://electronjs.org/headers
 npm run prune-node-modules
 npm run purge-build-artifacts
 npm run package-mac
