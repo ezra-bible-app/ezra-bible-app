d4f118de3de4df4713ec804d803c1022126be649
Tobias Klein <contact@tklein.info>
2026-01-11 22:05:15 +0100

    Update sqlite3 version to 5.1.7 in build script for macOS

--- build_scripts/macos/build_mac.sh
+++ build_scripts/macos/build_mac.sh
@@ -18,7 +18,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.10
 npm install --target-arch=$ARCH
 npm run compile-pug
 npm run commit-info
-npm install sqlite3@5.0.2 --target-arch=$ARCH --build-from-source --runtime=electron --target=32.2.3 --dist-url=https://electronjs.org/headers
+npm install sqlite3@5.1.7 --target-arch=$ARCH --build-from-source --runtime=electron --target=32.2.3 --dist-url=https://electronjs.org/headers
 npm run prune-node-modules
 npm run purge-build-artifacts
 npm run package-mac-$ARCH
