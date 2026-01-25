5a9184327bd58547b8e3d699099dff22db62de07
Tobias Klein <contact@tklein.info>
2026-01-25 17:19:54 +0100

    Fix: add delay before publishing generic panel switched event

--- app/frontend/components/tool_panel/panel_buttons.js
+++ app/frontend/components/tool_panel/panel_buttons.js
@@ -214,9 +214,13 @@ class PanelButtons extends HTMLElement {
       panelElement.classList.remove('active');
     }
 
+    // Publish generic panel switched event for global listeners with a delay of 100ms
+    setTimeout(async () => {
+      await eventController.publishAsync('on-panel-switched', setActive);
+    }, 100);
+
+    // Publish specific panel event
     await eventController.publishAsync(this.panelEvents[panelId], setActive);
-    // Also publish generic panel switched event for global listeners
-    await eventController.publishAsync('on-panel-switched', setActive);
   }
 
   _getButtonForPanel(panelId) {
