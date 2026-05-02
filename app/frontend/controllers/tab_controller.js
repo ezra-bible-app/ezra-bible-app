72e25c6e2d4656c4806dd588c44170d4a7ebf5bf
Tobias Klein <contact@tklein.info>
2026-05-02 10:20:08 +0200

    Move visibility toggle for Bible select title line and verse list to after loading completion

--- app/frontend/controllers/tab_controller.js
+++ app/frontend/controllers/tab_controller.js
@@ -399,8 +399,6 @@ class TabController {
 
       if (loadedTabCount > 0) {
         await this.populateFromMetaTabs(force);
-        $('[id="bible-select-title-line"]').css('visibility', '');
-        $('.verse-list').show();
         verseListController.hideVerseListLoadingIndicator();
         uiHelper.hideTextLoadingIndicator();
       } else {
@@ -428,6 +426,11 @@ class TabController {
     } else {
       // Call this method explicitly to initialize the first tab
       await eventController.publishAsync('on-tab-selected', 0);
+    }
+
+    if (loadedTabCount > 0) {
+      $('[id="bible-select-title-line"]').css('visibility', '');
+      $('.verse-list').show();
     }
 
     await waitUntilIdle();
